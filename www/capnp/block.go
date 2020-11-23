9f3cafe6aae9ff4caed6c70e3fc6e3bf5b4a75d1
mantre <mantre@protonmail.com>
2020-11-23 21:51:47 +0800

    Fix a crash on capnp on querying the first block

--- www/capnp/block.go
+++ www/capnp/block.go
@@ -23,13 +23,15 @@ func (f factory) BlockToBlockInfo(block *block.Block, height int, cbi *BlockInfo
 	cbi.SetHeight(uint32(height))
 	cbi.SetData(data)
 	// last commit
-	clc.SetRound(uint32(block.Header().LastCommit().Round()))
-	clc.SetSignature(block.Header().LastCommit().Signature().RawBytes())
-	clcc, _ := clc.NewCommiters(int32(len(block.Header().LastCommit().Commiters())))
-	for i, commiter := range block.Header().LastCommit().Commiters() {
-		c := clcc.At(i)
-		c.SetAddress(commiter.Address.RawBytes())
-		c.SetSigned(commiter.Signed)
+	if block.Header().LastCommit() != nil {
+		clc.SetRound(uint32(block.Header().LastCommit().Round()))
+		clc.SetSignature(block.Header().LastCommit().Signature().RawBytes())
+		clcc, _ := clc.NewCommiters(int32(len(block.Header().LastCommit().Commiters())))
+		for i, commiter := range block.Header().LastCommit().Commiters() {
+			c := clcc.At(i)
+			c.SetAddress(commiter.Address.RawBytes())
+			c.SetSigned(commiter.Signed)
+		}
 	}
 	// header
 	ch.SetVersion(int32(block.Header().Version()))
