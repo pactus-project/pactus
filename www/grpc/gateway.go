291383fa705a86989ace629a3f540f4e801ef726
Mostafa Sedaghat Joo <mostafa.sedaghat@gmail.com>
2021-05-25 21:11:21 +0800

    Update gateway.go
    fix linting error: unused parameter

--- www/grpc/gateway.go
+++ www/grpc/gateway.go
@@ -82,7 +82,7 @@ func (s *Server) startGateway() error {
 // preflightHandler adds the necessary headers in order to serve
 // CORS from any origin using the methods "GET", "HEAD", "POST", "PUT", "DELETE"
 // We insist, don't do this without consideration in production systems.
-func preflightHandler(w http.ResponseWriter, r *http.Request) {
+func preflightHandler(w http.ResponseWriter) {
 	headers := []string{"Content-Type", "Accept", "Authorization"}
 	w.Header().Set("Access-Control-Allow-Headers", strings.Join(headers, ","))
 	methods := []string{"GET", "HEAD", "POST", "PUT", "DELETE"}
@@ -96,7 +96,7 @@ func allowCORS(h http.Handler) http.Handler {
 		if origin := r.Header.Get("Origin"); origin != "" {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
-				preflightHandler(w, r)
+				preflightHandler(w)
 				return
 			}
 		}
