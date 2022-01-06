ce41395c782fe9d5b74b28221e4814abe2f35c8f
Mostafa <mostafa.sedaghat@gmail.com>
2022-01-06 12:08:35 +0800

    Removing legacy mDNS

--- network/mdns.go
+++ network/mdns.go
@@ -6,20 +6,17 @@ import (
 
 	lp2phost "github.com/libp2p/go-libp2p-core/host"
 	lp2ppeer "github.com/libp2p/go-libp2p-core/peer"
-	lp2pdiscovery "github.com/libp2p/go-libp2p/p2p/discovery/mdns_legacy"
+	lp2pmdns "github.com/libp2p/go-libp2p/p2p/discovery/mdns"
 )
 
-// DiscoveryInterval is how often we re-publish our mDNS records.
-const DiscoveryInterval = time.Hour
-
 // DiscoveryServiceTag is used in our mDNS advertisements to discover other peers.
-const DiscoveryServiceTag = "pubsub-zarb"
+const DiscoveryServiceTag = "mdns-zarb"
 
 // HandlePeerFound connects to peers discovered via mDNS. Once they're connected,
 // the PubSub system will automatically start interacting with them if they also
 // support PubSub.
 func (n *network) HandlePeerFound(pi lp2ppeer.AddrInfo) {
-	n.logger.Trace("discovered new peer", "id", pi.ID.Pretty())
+	n.logger.Debug("discovered new peer", "id", pi.ID.Pretty())
 	ctx, cancel := context.WithTimeout(n.ctx, time.Second*10)
 	defer cancel()
 	if err := n.host.Connect(ctx, pi); err != nil {
@@ -29,13 +26,14 @@ func (n *network) HandlePeerFound(pi lp2ppeer.AddrInfo) {
 
 // setupDiscovery creates an mDNS discovery service and attaches it to the libp2p Host.
 // This lets us automatically discover peers on the same LAN and connect to them.
-func (n *network) setupMNSDiscovery(ctx context.Context, h lp2phost.Host) (lp2pdiscovery.Service, error) {
+func (n *network) setupMNSDiscovery(h lp2phost.Host) (lp2pmdns.Service, error) {
 	// setup mDNS discovery to find local peers
-	service, err := lp2pdiscovery.NewMdnsService(ctx, h, DiscoveryInterval, DiscoveryServiceTag)
+	service := lp2pmdns.NewMdnsService(h, DiscoveryServiceTag, n)
+
+	err := service.Start()
 	if err != nil {
 		return nil, err
 	}
 
-	service.RegisterNotifee(n)
 	return service, nil
 }
