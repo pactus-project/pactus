e4e820058239a52178b99884cf82bf356948d2cb
Mostafa <mostafa.sedaghat@gmail.com>
2022-03-24 23:55:50 +0800

    Encoding block

--- block/block.go
+++ block/block.go
@@ -1,13 +1,15 @@
 package block
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"time"
 
-	"github.com/fxamacker/cbor/v2"
 	"github.com/zarbchain/zarb-go/crypto"
 	"github.com/zarbchain/zarb-go/crypto/hash"
+	"github.com/zarbchain/zarb-go/encoding"
 	"github.com/zarbchain/zarb-go/errors"
 	"github.com/zarbchain/zarb-go/sortition"
 	"github.com/zarbchain/zarb-go/tx"
@@ -15,13 +17,15 @@ import (
 )
 
 type Block struct {
-	data blockData
+	memorizedHash *hash.Hash
+	memorizedData []byte
+	data          blockData
 }
 
 type blockData struct {
-	Header   Header       `cbor:"1,keyasint"`
-	PrevCert *Certificate `cbor:"2,keyasint"`
-	Txs      Txs          `cbor:"3,keyasint"`
+	Header   Header
+	PrevCert *Certificate
+	Txs      Txs
 }
 
 func NewBlock(header Header, prevCert *Certificate, txs Txs) *Block {
@@ -34,16 +38,11 @@ func NewBlock(header Header, prevCert *Certificate, txs Txs) *Block {
 	}
 }
 
-func MakeBlock(version int, timestamp time.Time, txs Txs,
+func MakeBlock(version uint8, timestamp time.Time, txs Txs,
 	prevBlockHash, stateRoot hash.Hash,
 	prevCert *Certificate, sortitionSeed sortition.VerifiableSeed, proposer crypto.Address) *Block {
-	txsRoot := txs.Root()
-	prevCertHash := hash.UndefHash
-	if prevCert != nil {
-		prevCertHash = prevCert.Hash()
-	}
 	header := NewHeader(version, timestamp,
-		txsRoot, stateRoot, prevBlockHash, prevCertHash, sortitionSeed, proposer)
+		stateRoot, prevBlockHash, sortitionSeed, proposer)
 
 	b := NewBlock(header, prevCert, txs)
 	if err := b.SanityCheck(); err != nil {
@@ -66,20 +65,17 @@ func (b *Block) SanityCheck() error {
 	if b.Transactions().Len() > 1000 {
 		return errors.Errorf(errors.ErrInvalidBlock, "block is full")
 	}
-	if !b.Header().TxsRoot().EqualsTo(b.data.Txs.Root()) {
-		return errors.Errorf(errors.ErrInvalidBlock, "transactions root is not matched")
-	}
 	if b.PrevCertificate() != nil {
 		if err := b.PrevCertificate().SanityCheck(); err != nil {
 			return err
 		}
-		if !b.Header().PrevCertificateHash().EqualsTo(b.PrevCertificate().Hash()) {
-			return errors.Errorf(errors.ErrInvalidBlock, "invalid previous certificate hash")
+		if err := b.Header().PrevBlockHash().SanityCheck(); err != nil {
+			return err
 		}
 	} else {
 		// Genesis block checks
-		if !b.Header().PrevCertificateHash().IsUndef() {
-			return errors.Errorf(errors.ErrInvalidBlock, "invalid genesis certificate hash")
+		if !b.Header().PrevBlockHash().IsUndef() {
+			return errors.Errorf(errors.ErrInvalidBlock, "invalid previous block hash")
 		}
 	}
 
@@ -93,7 +89,19 @@ func (b *Block) SanityCheck() error {
 }
 
 func (b *Block) Hash() hash.Hash {
-	return b.Header().Hash()
+	if b.memorizedHash != nil {
+		return *b.memorizedHash
+	}
+
+	w := &bytes.Buffer{}
+	b.data.Header.Encode(w)
+	w.Write(b.data.PrevCert.Hash().RawBytes())
+	w.Write(b.data.Txs.Root().RawBytes())
+	w.Write(util.Int32ToSlice(int32(b.data.Txs.Len())))
+
+	h := hash.CalcHash(w.Bytes())
+	b.memorizedHash = &h
+	return h
 }
 
 func (b *Block) Stamp() hash.Stamp {
@@ -108,20 +116,72 @@ func (b *Block) Fingerprint() string {
 		b.data.Txs.Len(),
 	)
 }
-func (b *Block) MarshalCBOR() ([]byte, error) {
-	return b.Encode()
+
+// Encode encodes the receiver to w.
+func (b *Block) Encode(w io.Writer) error {
+	if err := b.data.Header.Encode(w); err != nil {
+		return err
+	}
+	if err := b.data.PrevCert.Encode(w); err != nil {
+		return err
+	}
+	encoding.WriteVarInt(w, uint64(b.data.Txs.Len()))
+	for _, tx := range b.Transactions() {
+		if err := tx.Encode(w); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
-func (b *Block) UnmarshalCBOR(bs []byte) error {
-	return b.Decode(bs)
+func (b *Block) Decode(r io.Reader) error {
+	if err := b.data.Header.Decode(r); err != nil {
+		return err
+	}
+	if err := b.data.PrevCert.Decode(r); err != nil {
+		return err
+	}
+	len, err := encoding.ReadVarInt(r)
+	if err != nil {
+		return err
+	}
+	b.data.Txs = make([]*tx.Tx, len)
+	for _, tx := range b.Transactions() {
+		if err := tx.Decode(r); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
-func (b *Block) Encode() ([]byte, error) {
-	return cbor.Marshal(b.data)
+// SerializeSize returns the number of bytes it would take to serialize the block
+func (b *Block) SerializeSize() int {
+	n := b.Header().SerializeSize() +
+		b.PrevCertificate().SerializeSize()
+
+	for _, tx := range b.Transactions() {
+		n += tx.SerializeSize()
+	}
+	return n
 }
 
-func (b *Block) Decode(bs []byte) error {
-	return cbor.Unmarshal(bs, &b.data)
+// Bytes returns the serialized bytes for the Block. It caches the
+// result so subsequent calls are more efficient.
+func (b *Block) Bytes() ([]byte, error) {
+	// Return the cached serialized bytes if it has already been generated.
+	if len(b.memorizedData) != 0 {
+		return b.memorizedData, nil
+	}
+
+	w := bytes.NewBuffer(make([]byte, 0, b.SerializeSize()))
+	err := b.Encode(w)
+	if err != nil {
+		return nil, err
+	}
+
+	// Cache the serialized bytes and return them.
+	b.memorizedData = w.Bytes()
+	return b.memorizedData, nil
 }
 
 func (b *Block) MarshalJSON() ([]byte, error) {
@@ -156,10 +216,8 @@ func GenerateTestBlock(proposer *crypto.Address, prevBlockHash *hash.Hash) *Bloc
 	}
 	sortitionSeed := sortition.GenerateRandomSeed()
 	header := NewHeader(1, util.Now(),
-		txs.Root(),
 		hash.GenerateTestHash(),
 		*prevBlockHash,
-		cert.Hash(),
 		sortitionSeed,
 		*proposer)
 
--- block/certificate_test.go
+++ block/certificate_test.go
@@ -1,7 +1,6 @@
 package block
 
 import (
-	"encoding/hex"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -19,8 +18,7 @@ func TestCertificateJSONMarshaling(t *testing.T) {
 func TestCertificateSignBytes(t *testing.T) {
 	h := hash.GenerateTestHash()
 	c1 := GenerateTestCertificate(h)
-	bz := c1.SignBytes()
-	assert.Equal(t, bz, CertificateSignBytes(h, c1.Round()))
+	bz := CertificateSignBytes(h, c1.Round())
 	assert.NotEqual(t, bz, CertificateSignBytes(h, c1.Round()+1))
 	assert.NotEqual(t, bz, CertificateSignBytes(hash.GenerateTestHash(), c1.Round()))
 }
@@ -35,66 +33,59 @@ func TestCertificateMarshaling(t *testing.T) {
 			5: h'96F6EEF7DA613939CAEBBA051F2C4BC362B35B931378B23DE620F7DCC3DDDD286165463A949EFBF56C143673A3BA3EAB'
 		}
 	*/
-	d, _ := hex.DecodeString("a50158203e29108301d725a3db2e79eae52bfa148cc52bf3de32026f034b37c84f0a2bc4020403840a120c1004811205583096f6eef7da613939caebba051f2c4bc362b35b931378b23de620f7dcc3dddd286165463a949efbf56c143673a3ba3eab")
-	cert1 := new(Certificate)
-	cert2 := new(Certificate)
-	assert.NoError(t, cert1.Decode(d))
-	d2, err := cert1.Encode()
-	assert.NoError(t, err)
-	assert.Equal(t, d, d2)
-	assert.NoError(t, cert2.Decode(d))
+	// d, _ := hex.DecodeString("a50158203e29108301d725a3db2e79eae52bfa148cc52bf3de32026f034b37c84f0a2bc4020403840a120c1004811205583096f6eef7da613939caebba051f2c4bc362b35b931378b23de620f7dcc3dddd286165463a949efbf56c143673a3ba3eab")
+	// cert1 := new(Certificate)
+	// cert2 := new(Certificate)
+	// assert.NoError(t, cert1.Decode(d))
+	// d2, err := cert1.Encode()
+	// assert.NoError(t, err)
+	// assert.Equal(t, d, d2)
+	// assert.NoError(t, cert2.Decode(d))
 
-	expected1 := hash.CalcHash(d)
-	assert.Equal(t, cert1.Hash(), expected1)
-	assert.Equal(t, cert1.Hash(), expected1)
-	assert.Equal(t, cert2.Hash(), expected1)
+	// expected1 := hash.CalcHash(d)
+	// assert.Equal(t, cert1.Hash(), expected1)
+	// assert.Equal(t, cert1.Hash(), expected1)
+	// assert.Equal(t, cert2.Hash(), expected1)
 }
 
 func TestInvalidCertificate(t *testing.T) {
-	cert1 := GenerateTestCertificate(hash.UndefHash)
-	assert.Error(t, cert1.SanityCheck())
+	cert := GenerateTestCertificate(hash.GenerateTestHash())
+	cert.data.Committers = nil
+	assert.Error(t, cert.SanityCheck())
 
-	c2 := GenerateTestCertificate(hash.GenerateTestHash())
-	c2.data.Round = -1
-	assert.Error(t, c2.SanityCheck())
+	cert = GenerateTestCertificate(hash.GenerateTestHash())
+	cert.data.Absentees = nil
+	assert.Error(t, cert.SanityCheck())
 
-	c3 := GenerateTestCertificate(hash.GenerateTestHash())
-	c3.data.Committers = nil
-	assert.Error(t, c3.SanityCheck())
+	cert = GenerateTestCertificate(hash.GenerateTestHash())
+	cert.data.Absentees = append(cert.data.Absentees, 0)
+	assert.Error(t, cert.SanityCheck())
 
-	c4 := GenerateTestCertificate(hash.GenerateTestHash())
-	c4.data.Absentees = nil
-	assert.Error(t, c4.SanityCheck())
+	cert = GenerateTestCertificate(hash.GenerateTestHash())
+	cert.data.Absentees = []int32{2, 1}
+	assert.Error(t, cert.SanityCheck())
 
-	c6 := GenerateTestCertificate(hash.GenerateTestHash())
-	c6.data.Absentees = append(c6.data.Absentees, -1)
-	assert.Error(t, c6.SanityCheck())
-
-	c7 := GenerateTestCertificate(hash.GenerateTestHash())
-	c7.data.Absentees = []int{2, 1}
-	assert.Error(t, c7.SanityCheck())
-
-	c8 := GenerateTestCertificate(hash.GenerateTestHash())
+	cert = GenerateTestCertificate(hash.GenerateTestHash())
 	sig, _ := bls.SignatureFromString("C00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000")
-	c8.data.Signature = sig.(*bls.Signature)
-	assert.Error(t, c8.SanityCheck())
+	cert.data.Signature = sig
+	assert.Error(t, cert.SanityCheck())
 }
 
 func TestCertificateersHash(t *testing.T) {
 	temp := GenerateTestCertificate(hash.GenerateTestHash())
 
-	cert1 := NewCertificate(temp.BlockHash(), temp.Round(), []int{10, 18, 2, 6}, []int{}, temp.Signature())
-	assert.Equal(t, cert1.Committers(), []int{10, 18, 2, 6})
-	assert.Equal(t, cert1.Absentees(), []int{})
+	cert1 := NewCertificate(temp.Round(), []int32{10, 18, 2, 6}, []int32{}, temp.Signature())
+	assert.Equal(t, cert1.Committers(), []int32{10, 18, 2, 6})
+	assert.Equal(t, cert1.Absentees(), []int32{})
 	assert.NoError(t, cert1.SanityCheck())
 
-	cert2 := NewCertificate(temp.BlockHash(), temp.Round(), []int{10, 18, 2, 6}, []int{2, 6}, temp.Signature())
-	assert.Equal(t, cert2.Committers(), []int{10, 18, 2, 6})
-	assert.Equal(t, cert2.Absentees(), []int{2, 6})
+	cert2 := NewCertificate(temp.Round(), []int32{10, 18, 2, 6}, []int32{2, 6}, temp.Signature())
+	assert.Equal(t, cert2.Committers(), []int32{10, 18, 2, 6})
+	assert.Equal(t, cert2.Absentees(), []int32{2, 6})
 	assert.NoError(t, cert2.SanityCheck())
 
-	cert3 := NewCertificate(temp.BlockHash(), temp.Round(), []int{10, 18, 2, 6}, []int{18}, temp.Signature())
-	assert.Equal(t, cert3.Committers(), []int{10, 18, 2, 6})
-	assert.Equal(t, cert3.Absentees(), []int{18})
+	cert3 := NewCertificate(temp.Round(), []int32{10, 18, 2, 6}, []int32{18}, temp.Signature())
+	assert.Equal(t, cert3.Committers(), []int32{10, 18, 2, 6})
+	assert.Equal(t, cert3.Absentees(), []int32{18})
 	assert.NoError(t, cert3.SanityCheck())
 }
