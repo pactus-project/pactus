1c485cac5d55dd31db2be4d4887f4588d64916bc
Mostafa <mostafa.sedaghat@gmail.com>
2022-04-15 02:32:00 +0800

    testing releaser.sh

--- .github/workflows/releaser.sh
+++ .github/workflows/releaser.sh
@@ -7,9 +7,27 @@ TAG=`git describe --abbrev=0 --tags`
 
 make release
 
+## Releasing GUI app
+if [[ "$OS" == "Linux" ]]; then
+sudo apt install libgtk-3-dev libcairo2-dev libglib2.0-dev
+else if  [[ "$OS" == "Darwin" ]]; then
+brew install gobject-introspection gtk+3 adwaita-icon-theme
+else if  [[ "$OS" == "mingw"* ]]; then
+pacman -Sy --noconfirm git \
+    mingw-w64-x86_64-gtk3 \
+    mingw-w64-x86_64-toolchain \
+    mingw-w64-x86_64-go \
+    mingw-w64-x86_64-pkg-config \
+    mingw-w64-x86_64-gcc \
+    base-devel \
+    glib2-devel
+
+go build -ldflags "-s -w" -tags glib_2_66,gtk -o ./build/zarb-gui.exe ./cmd/gtk
+fi
+
 if [[ "$OS" == "mingw"* ]]; then
     OS="windows"
-    7z a zarb-windows-$TAG-$MACH.zip zarb-daemon.exe zarb-wallet.exe
+    7z a zarb-windows-$TAG-$MACH.zip zarb-daemon.exe zarb-wallet.exe zarb-gui.exe
 else
-    tar -czvf zarb-$OS-$TAG-$MACH.tar.gz zarb-daemon zarb-wallet
+    tar -czvf zarb-$OS-$TAG-$MACH.tar.gz ./build/zarb-daemon ./build/zarb-wallet ./build/zarb-gui
 fi
