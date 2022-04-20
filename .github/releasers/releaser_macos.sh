230603185dd1c86736bb6c67ab6a82c332661907
Payam Khaninejad <khaninejad@gmail.com>
2022-04-20 13:11:25 +0430

    Update release workflow to support macos dmg installer (#302)
    * update releaser for support dmg
    
    * add macos apps schema
    
    * update macos build for support dmg

--- .github/releasers/releaser_macos.sh
+++ .github/releasers/releaser_macos.sh
@@ -12,6 +12,52 @@ echo "VERSION: $VERSION"
 echo "ROOT_DIR: $ROOT_DIR"
 echo "PACKAGE_DIR: $PACKAGE_DIR"
 
-mkdir ${PACKAGE_DIR}
-
 echo "Building the binaries"
+
+cd ${ROOT_DIR}
+make herumi
+export CGO_LDFLAGS="-L.herumi/bls/lib -lbls384_256 -lm -lstdc++ -g -O2"
+go build -ldflags "-s -w" -o ${BUILD_DIR}/zarb-daemon ./cmd/daemon
+go build -ldflags "-s -w" -o ${BUILD_DIR}/zarb-wallet ./cmd/wallet
+go build -ldflags "-s -w" -tags gtk -o ${BUILD_DIR}/zarb-gui ./cmd/gtk
+
+
+
+echo "Installing gtk-mac-bundler"
+git clone https://gitlab.gnome.org/GNOME/gtk-mac-bundler.git
+cd gtk-mac-bundler
+make install
+BUNDLER=${HOME}/.local/bin/gtk-mac-bundler
+cd -
+
+echo "Bundling the GUI package"
+GUI_BUNDLE=${ROOT_DIR}/gui-bundle
+mkdir ${GUI_BUNDLE}
+
+cp ${BUILD_DIR}/zarb-gui                  ${GUI_BUNDLE}
+cp ${ROOT_DIR}/.github/releasers/macos/*  ${GUI_BUNDLE}
+
+# https://stackoverflow.com/questions/21242932/sed-i-may-not-be-used-with-stdin-on-mac-os-x
+sed -i '' "s/%SHORTVERSION%/${VERSION}/"     ${GUI_BUNDLE}/Info.plist
+sed -i '' "s/%VERSION%/Version ${VERSION}/"  ${GUI_BUNDLE}/Info.plist
+
+export GUI_BUNDLE
+export ROOT_DIR
+
+${BUNDLER} ${GUI_BUNDLE}/gui.bundle
+
+
+echo "Creating dmg"
+# https://github.com/create-dmg/create-dmg
+create-dmg \
+  --volname "Zarb GUI" \
+  "${PACKAGE_NAME}-osx-64.dmg" \
+  "${ROOT_DIR}/zarb-gui.app"
+
+echo "Creating archive"
+mkdir ${PACKAGE_DIR}
+cp ${BUILD_DIR}/zarb-daemon     ${PACKAGE_DIR}
+cp ${BUILD_DIR}/zarb-wallet     ${PACKAGE_DIR}
+cp -R ${ROOT_DIR}/zarb-gui.app  ${PACKAGE_DIR}
+
+tar -czvf ${ROOT_DIR}/${PACKAGE_NAME}-osx-64.tar.gz -p ${PACKAGE_NAME}
