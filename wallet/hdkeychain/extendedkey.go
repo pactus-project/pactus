9adefcbd3579935460d3c89e87c29f1d05e54c41
Mostafa <mostafa.sedaghat@gmail.com>
2022-07-06 18:07:59 +0800

    hd key based on BIP32

---
+++ wallet/hdkeychain/extendedkey.go
@@ -0,0 +1,435 @@
+package hdkeychain
+
+// References:
+//   [BIP32]: BIP0032 - Hierarchical Deterministic Wallets
+//   https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki
+
+import (
+	"crypto/hmac"
+	"crypto/rand"
+	"crypto/sha512"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"math/big"
+
+	herumi "github.com/herumi/bls-go-binary/bls"
+	"github.com/zarbchain/zarb-go/types/crypto"
+	"github.com/zarbchain/zarb-go/types/crypto/bls"
+)
+
+var curveOrder big.Int
+var g2Gen herumi.G2
+
+func init() {
+	err := g2Gen.SetString(`
+	1
+	24aa2b2f08f0a91260805272dc51051c6e47ad4fa403b02b4510b647ae3d1770bac0326a805bbefd48056c8c121bdb8
+	13e02b6052719f607dacd3a088274f65596bd0d09920b61ab5da61bbdc7f5049334cf11213945d57e5ac7d055d042b7e
+	ce5d527727d6e118cc9cdc6da2e351aadfd9baa8cbdd3a76d429a695160d12c923ac9cc3baca289e193548608b82801
+	606c4a02ea734cc32acd2b02bc28b99cb3e287e85a763af267492ab572e99ab3f370d275cec1da1aaa9075ff05f79be`, 16)
+
+	if err != nil {
+		panic(err)
+	}
+
+	curveOrder.SetString("73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001", 16)
+}
+
+const (
+	// HardenedKeyStart is the index at which a hardened key starts.  Each
+	// extended key has 2^31 normal child keys and 2^31 hardened child keys.
+	// Thus the range for normal child keys is [0, 2^31 - 1] and the range
+	// for hardened child keys is [2^31, 2^32 - 1].
+	HardenedKeyStart = 0x80000000 // 2^31
+
+	// MinSeedBytes is the minimum number of bytes allowed for a seed to
+	// a master node.
+	MinSeedBytes = 16 // 128 bits
+
+	// MaxSeedBytes is the maximum number of bytes allowed for a seed to
+	// a master node.
+	MaxSeedBytes = 64 // 512 bits
+
+	// maxUint8 is the max positive integer which can be serialized in a uint8
+	maxUint8 = 1<<8 - 1
+)
+
+var (
+	// ErrDeriveHardFromPublic describes an error in which the caller
+	// attempted to derive a hardened extended key from a public key.
+	ErrDeriveHardFromPublic = errors.New("cannot derive a hardened key " +
+		"from a public key")
+
+	// ErrDeriveBeyondMaxDepth describes an error in which the caller
+	// has attempted to derive more than 255 keys from a root key.
+	ErrDeriveBeyondMaxDepth = errors.New("cannot derive a key with more than " +
+		"255 indices in its path")
+
+	// ErrNotPrivExtKey describes an error in which the caller attempted
+	// to extract a private key from a public extended key.
+	ErrNotPrivExtKey = errors.New("unable to create private keys from a " +
+		"public extended key")
+
+	// ErrInvalidChild describes an error in which the child at a specific
+	// index is invalid due to the derived key falling outside of the valid
+	// range for secp256k1 private keys.  This error indicates the caller
+	// should simply ignore the invalid child extended key at this index and
+	// increment to the next index.
+	ErrInvalidChild = errors.New("the extended key at this index is invalid")
+
+	// ErrUnusableSeed describes an error in which the provided seed is not
+	// usable due to the derived key falling outside of the valid range for
+	// secp256k1 private keys.  This error indicates the caller must choose
+	// another seed.
+	ErrUnusableSeed = errors.New("unusable seed")
+
+	// ErrInvalidSeedLen describes an error in which the provided seed or
+	// seed length is not in the allowed range.
+	ErrInvalidSeedLen = fmt.Errorf("seed length must be between %d and %d "+
+		"bits", MinSeedBytes*8, MaxSeedBytes*8)
+)
+
+// masterKey is the master key used along with a random seed used to generate
+// the master node in the hierarchical tree.
+var masterKey = []byte("Zarb seed")
+
+// ExtendedKey houses all the information needed to support a hierarchical
+// deterministic extended key.  See the package overview documentation for
+// more details on how to use extended keys.
+type ExtendedKey struct {
+	key       []byte // This will be the pubkey for extended pub keys
+	pubKey    []byte // This will only be set for extended priv keys
+	chainCode []byte
+	depth     uint8
+	childNum  uint32
+	isPrivate bool
+}
+
+// NewExtendedKey returns a new instance of an extended key with the given
+// fields.  No error checking is performed here as it's only intended to be a
+// convenience method used to create a populated struct. This function should
+// only be used by applications that need to create custom ExtendedKeys. All
+// other applications should just use NewMaster, Derive, or Neuter.
+func NewExtendedKey(key, chainCode []byte, depth uint8,
+	childNum uint32, isPrivate bool) *ExtendedKey {
+
+	// NOTE: The pubKey field is intentionally left nil so it is only
+	// computed and memoized as required.
+	return &ExtendedKey{
+		key:       key,
+		pubKey:    nil,
+		chainCode: chainCode,
+		depth:     depth,
+		childNum:  childNum,
+		isPrivate: isPrivate,
+	}
+}
+
+// pubKeyBytes returns bytes for the serialized compressed public key associated
+// with this extended key in an efficient manner including memoization as
+// necessary.
+//
+// When the extended key is already a public key, the key is simply returned as
+// is since it's already in the correct form.  However, when the extended key is
+// a private key, the public key will be calculated and memoized so future
+// accesses can simply return the cached result.
+func (k *ExtendedKey) pubKeyBytes() []byte {
+	// Just return the key if it's already an extended public key.
+	if !k.isPrivate {
+		return k.key
+	}
+
+	// This is a private extended key, so calculate and memoize the public
+	// key if needed.
+	if len(k.pubKey) == 0 {
+		privKey := new(herumi.Fr)
+		pubPoint := new(herumi.G2)
+		privKey.Deserialize(k.key)
+
+		herumi.G2Mul(pubPoint, &g2Gen, privKey)
+		k.pubKey = pubPoint.Serialize()
+	}
+
+	return k.pubKey
+}
+
+// IsPrivate returns whether or not the extended key is a private extended key.
+//
+// A private extended key can be used to derive both hardened and non-hardened
+// child private and public extended keys.  A public extended key can only be
+// used to derive non-hardened child public extended keys.
+func (k *ExtendedKey) IsPrivate() bool {
+	return k.isPrivate
+}
+
+// Derive returns a derived child extended key at the given index.
+//
+// IMPORTANT: if you were previously using the Child method, this method is incompatible.
+// The Child method had a BIP-32 standard compatibility issue.  You have to check whether
+// any hardened derivations in your derivation path are affected by this issue, via the
+// IsAffectedByIssue172 method and migrate the wallet if so.  This method does conform
+// to the standard.  If you need the old behavior, use DeriveNonStandard.
+//
+// When this extended key is a private extended key (as determined by the IsPrivate
+// function), a private extended key will be derived.  Otherwise, the derived
+// extended key will be also be a public extended key.
+//
+// When the index is greater to or equal than the HardenedKeyStart constant, the
+// derived extended key will be a hardened extended key.  It is only possible to
+// derive a hardened extended key from a private extended key.  Consequently,
+// this function will return ErrDeriveHardFromPublic if a hardened child
+// extended key is requested from a public extended key.
+//
+// A hardened extended key is useful since, as previously mentioned, it requires
+// a parent private extended key to derive.  In other words, normal child
+// extended public keys can be derived from a parent public extended key (no
+// knowledge of the parent private key) whereas hardened extended keys may not
+// be.
+//
+// NOTE: There is an extremely small chance (< 1 in 2^127) the specific child
+// index does not derive to a usable child.  The ErrInvalidChild error will be
+// returned if this should occur, and the caller is expected to ignore the
+// invalid child and simply increment to the next index.
+func (k *ExtendedKey) Derive(i uint32) (*ExtendedKey, error) {
+	// Prevent derivation of children beyond the max allowed depth.
+	if k.depth == maxUint8 {
+		return nil, ErrDeriveBeyondMaxDepth
+	}
+
+	// There are four scenarios that could happen here:
+	// 1) Private extended key -> Hardened child private extended key
+	// 2) Private extended key -> Non-hardened child private extended key
+	// 3) Public extended key -> Non-hardened child public extended key
+	// 4) Public extended key -> Hardened child public extended key (INVALID!)
+
+	// Case #4 is invalid, so error out early.
+	// A hardened child extended key may not be created from a public
+	// extended key.
+	isChildHardened := i >= HardenedKeyStart
+	if !k.isPrivate && isChildHardened {
+		return nil, ErrDeriveHardFromPublic
+	}
+
+	// The data used to derive the child key depends on whether or not the
+	// child is hardened per [BIP32].
+	//
+	// For hardened children:
+	//   0x00 || ser256(parentKey) || ser32(i)
+	//
+	// For normal children:
+	//   serP(parentPubKey) || ser32(i)
+	keyLen := 33
+	data := make([]byte, keyLen+4)
+	if isChildHardened {
+		// Case #1.
+		// When the child is a hardened child, the key is known to be a
+		// private key due to the above early return.  Pad it with a
+		// leading zero as required by [BIP32] for deriving the child.
+		// Additionally, right align it if it's shorter than 32 bytes.
+		offset := 33 - len(k.key)
+		copy(data[offset:], k.key)
+	} else {
+		// Case #2 or #3.
+		// This is either a public or private extended key, but in
+		// either case, the data which is used to derive the child key
+		// starts with the secp256k1 compressed public key bytes.
+		copy(data, k.pubKeyBytes())
+	}
+	binary.BigEndian.PutUint32(data[keyLen:], i)
+
+	// Take the HMAC-SHA512 of the current key's chain code and the derived
+	// data:
+	//   I = HMAC-SHA512(Key = chainCode, Data = data)
+	hmac512 := hmac.New(sha512.New, k.chainCode)
+	_, _ = hmac512.Write(data)
+	ilr := hmac512.Sum(nil)
+
+	// Split "I" into two 32-byte sequences Il and Ir where:
+	//   Il = intermediate key used to derive the child
+	//   Ir = child chain code
+	il := ilr[:len(ilr)/2]
+	childChainCode := ilr[len(ilr)/2:]
+
+	// Both derived public or private keys rely on treating the left 32-byte
+	// sequence calculated above (Il) as a 256-bit integer that must be
+	// within the valid range for a secp256k1 private key.  There is a small
+	// chance (< 1 in 2^127) this condition will not hold, and in that case,
+	// a child extended key can't be created for this index and the caller
+	// should simply increment to the next index.
+	ilNum := new(big.Int)
+	ilNum.SetBytes(il)
+
+	// The algorithm used to derive the child key depends on whether or not
+	// a private or public child is being derived.
+	//
+	// For private children:
+	//   childKey = parse256(Il) + parentKey
+	//
+	// For public children:
+	//   childKey = serP(point(parse256(Il)) + parentKey)
+	var isPrivate bool
+	var childKey []byte
+	if k.isPrivate {
+		// Case #1 or #2.
+		// Add the parent private key to the intermediate private key to
+		// derive the final child key.
+		//
+		// childKey = parse256(Il) + parenKey
+		keyNum := new(big.Int)
+		keyNum.SetBytes(k.key)
+
+		sum := new(big.Int)
+		sum.Add(ilNum, keyNum)
+
+		reduced := new(big.Int)
+		reduced.Mod(sum, &curveOrder)
+		childKey = reduced.Bytes()
+
+		// Strip leading zeroes from childKey, to match the expectation
+		// as the old big.Int usage in this area of the codebase.
+		for len(childKey) > 0 && childKey[0] == 0x00 {
+			panic("what is this for?")
+			childKey = childKey[1:]
+		}
+
+		isPrivate = true
+	} else {
+		// Case #3.
+		// Calculate the corresponding intermediate public key for the
+		// intermediate private key: ilJ = ilScalar*G
+		ilScalar := new(herumi.Fr)
+		ilScalar.SetLittleEndian(il)
+
+		ilPoint := new(herumi.G2)
+
+		herumi.G2Mul(ilPoint, &g2Gen, ilScalar)
+
+		if (ilPoint.X.IsZero() && ilPoint.Y.IsZero()) || ilPoint.Z.IsZero() {
+			return nil, ErrInvalidChild
+		}
+
+		// Convert the serialized compressed parent public key into X
+		// and Y coordinates so it can be added to the intermediate
+		// public key.
+
+		pubKey := new(herumi.G2)
+		err := pubKey.Deserialize(k.key)
+		if err != nil {
+			return nil, err
+		}
+
+		// Convert the public key to jacobian coordinates, as that's
+		// what our main add/double methods use.
+		childPubKey := new(herumi.G2)
+		herumi.G2Add(childPubKey, pubKey, ilPoint)
+
+		childKey = childPubKey.Serialize()
+	}
+
+	return NewExtendedKey(childKey, childChainCode,
+		k.depth+1, i, isPrivate), nil
+}
+
+// ChildNum returns the index at which the child extended key was derived.
+//
+// Extended keys with ChildNum value between 0 and 2^31-1 are normal child
+// keys, and those with a value between 2^31 and 2^32-1 are hardened keys.
+func (k *ExtendedKey) ChildIndex() uint32 {
+	return k.childNum
+}
+
+// Neuter returns a new extended public key from this extended private key.  The
+// same extended key will be returned unaltered if it is already an extended
+// public key.
+//
+// As the name implies, an extended public key does not have access to the
+// private key, so it is not capable of signing transactions or deriving
+// child extended private keys.  However, it is capable of deriving further
+// child extended public keys.
+func (k *ExtendedKey) Neuter() (*ExtendedKey, error) {
+	// Already an extended public key.
+	if !k.isPrivate {
+		return k, nil
+	}
+
+	// Convert it to an extended public key.  The key for the new extended
+	// key will simply be the pubkey of the current extended private key.
+	//
+	// This is the function N((k,c)) -> (K, c) from [BIP32].
+	return NewExtendedKey(k.pubKeyBytes(), k.chainCode,
+		k.depth, k.childNum, false), nil
+}
+
+// BLSPublicKey converts the extended key to a BLS public key and returns it.
+func (k *ExtendedKey) BLSPublicKey() (*bls.PublicKey, error) {
+	return bls.PublicKeyFromBytes(k.pubKeyBytes())
+}
+
+// BLSPrivateKey converts the extended key to a BLS private key and returns it.
+// As you might imagine this is only possible if the extended key is a private
+// extended key (as determined by the IsPrivate function).  The ErrNotPrivExtKey
+// error will be returned if this function is called on a public extended key.
+func (k *ExtendedKey) BLSPrivateKey() (*bls.PrivateKey, error) {
+	if !k.isPrivate {
+		return nil, ErrNotPrivExtKey
+	}
+
+	return bls.PrivateKeyFromBytes(k.key)
+}
+
+// Address converts the extended key to a standard bitcoin pay-to-pubkey-hash
+// address for the passed network.
+func (k *ExtendedKey) Address() crypto.Address {
+	pub, _ := k.BLSPublicKey()
+	return pub.Address()
+}
+
+// NewMaster creates a new master node for use in creating a hierarchical
+// deterministic key chain.  The seed must be between 128 and 512 bits and
+// should be generated by a cryptographically secure random generation source.
+//
+func NewMaster(seed []byte) (*ExtendedKey, error) {
+	// Per [BIP32], the seed must be in range [MinSeedBytes, MaxSeedBytes].
+	if len(seed) < MinSeedBytes || len(seed) > MaxSeedBytes {
+		return nil, ErrInvalidSeedLen
+	}
+
+	// First take the HMAC-SHA512 of the master key and the seed data:
+	//   I = HMAC-SHA512(Key = "Bitcoin seed", Data = S)
+	hmac512 := hmac.New(sha512.New, masterKey)
+	_, _ = hmac512.Write(seed)
+	lr := hmac512.Sum(nil)
+
+	// Split "I" into two 32-byte sequences Il and Ir where:
+	//   Il = master secret key
+	//   Ir = master chain code
+	secretKey := lr[:len(lr)/2]
+	chainCode := lr[len(lr)/2:]
+
+	reduced := new(big.Int).SetBytes(secretKey)
+	reduced.Mod(reduced, &curveOrder)
+
+	return NewExtendedKey(reduced.Bytes(), chainCode, 0, 0, true), nil
+}
+
+// GenerateSeed returns a cryptographically secure random seed that can be used
+// as the input for the NewMaster function to generate a new master node.
+//
+// The length is in bytes and it must be between 16 and 64 (128 to 512 bits).
+// The recommended length is 32 (256 bits) as defined by the RecommendedSeedLen
+// constant.
+func GenerateSeed(length uint8) ([]byte, error) {
+	// Per [BIP32], the seed must be in range [MinSeedBytes, MaxSeedBytes].
+	if length < MinSeedBytes || length > MaxSeedBytes {
+		return nil, ErrInvalidSeedLen
+	}
+
+	buf := make([]byte, length)
+	_, err := rand.Read(buf)
+	if err != nil {
+		return nil, err
+	}
+
+	return buf, nil
+}
---
+++ wallet/hdkeychain/extendedkey_test.go
@@ -0,0 +1,196 @@
+package hdkeychain
+
+import (
+	"encoding/hex"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// TestNonHardenedDerivation tests several vectors which derive private keys in
+// non hardened mode
+func TestNonHardenedDerivation(t *testing.T) {
+	testSeed, _ := hex.DecodeString("c7195bf60f19a6ceee5bc4920ff6acf66d64126d743baa27bb9ead164c9b393dd3fc087ae9eafd83e32cb57f24dd653f5d4045dc5f45a4b461fb520cd8a10ca3")
+
+	tests := []struct {
+		name     string
+		path     []uint32
+		wantPriv string
+		wantPub  string
+	}{
+		// Test vector 1
+		{
+			name:     "test vector 1 chain m",
+			path:     []uint32{},
+			wantPriv: "SECRET1PPHL58434CM687WHZ0CDL2A36KDFRQ9GZS8SXJHX90E25W6DH6V3Q8SJRYL",
+			wantPub:  "public1pjhtucuyxzsmy526l5yjffj9ddt0lj05rhgk4rt7hyxrxhj27sdtc6qd8gxkseexlpfnjyd9jp2r7zqsy87mh72dtl9s2egmftkxgsc63rfj2yntedhz9xvx5smc8sqvhswzdv4a5zzmknzre4qz4cw2urupv25wf",
+		},
+		{
+			name:     "test vector 1 chain m/0",
+			path:     []uint32{0},
+			wantPriv: "SECRET1PR83YEN7X5NQMUTQFZJYDXKELCUL4R48GCTC3W5L9JDHEDU2HVCWS5JA5H7",
+			wantPub:  "public1pny43ff27stg5uvj8n6wd59aw54su9ngddg5lyn8g280kcyxlp3vjq7w4a5tz75ekmf8pslgkvrnryyryfwld7a7eq6rxrenw06gf5a69xkg8rkmew7lk3e5445dfcnf3v06fg4zcnqju6cum8m7fej47xc3pke58",
+		},
+		{
+			name:     "test vector 1 chain m/0/1",
+			path:     []uint32{0, 1},
+			wantPriv: "SECRET1PF9W7HJ85Y2LXJ3U2EN86TFY6YPA0KPV28F7UKCTHMT87J5C9F86Q9826VN",
+			wantPub:  "public1p5e9desagtctlq72f5sncjkf3a8wm3g9w6jz6grjq337rwx2zfqfg79kx4df2e292z0457ap0pjegz9mar87acpg3alr2tlr3hh8pjvuyelhhmhzn4qlyyf85p6eljlxf7fvzy0uf60e2nefajyqum0tpjurxvvs4",
+		},
+		{
+			name:     "test vector 1 chain m/0/1/2",
+			path:     []uint32{0, 1, 2},
+			wantPriv: "SECRET1PDU6VZEEDUW6KLATRTEK6CTEZCVDRHTJKZC4NM5GC08VSDFDR93XS7DJE4Z",
+			wantPub:  "public1p46264agws63z8rc9678vkytpjtyq4vhd05qp6f4njdgkg3k2uwrm99m75mmrep56me3acq390c2569lx7krghct8fnyldkcjacplnk4t7zw7h3r2ftadkdt0wyjdzjk6pdkerewqn6snygvsknkfjcajcvzluaxk",
+		},
+		{
+			name:     "test vector 1 chain m/0/1/2/2",
+			path:     []uint32{0, 1, 2, 2},
+			wantPriv: "SECRET1P88ZLGQ3QQUNKRA32302VC7ZPQYXYXRZAVY7HQCWL53YLCCG52EZS29HPCK",
+			wantPub:  "public1pnx2jce5y68sl7rxjndquy5w324dklc6a2f5rs73l5slwcfhp3zklr9aywnt3f4zyl7lgllpcv7e0qq7wlhrkanlxxgt3xhprf6mfke63lqx45mrznkl7yakd46hkqce0vx27afng93x7ddvl3jq82yv9cuadqcgl",
+		},
+		{
+			name:     "test vector 1 chain m/0/1/2/2/1000000000",
+			path:     []uint32{0, 1, 2, 2, 1000000000},
+			wantPriv: "SECRET1P87P43XJQVD4ANFD9QFDD0J34A8ZSSTFYJKMKEW53C8NQEMLTVH0SRY2G0P",
+			wantPub:  "public1pswx2u5xqxw3p223wxpcql2p5082kvdc879n7f9f26xwq2eqxc2v8pgwazazzqk09lpcrw2r030dxkyswc3ggmulkjzlzxaaaglc8yex822q39d5tfjerz5769nqlwp0qwaks2qgnv6g8dft55f0t9w38uscadmsk",
+		},
+	}
+
+	extKey, _ := NewMaster(testSeed)
+	extKey2, _ := NewMaster(testSeed)
+	neuterKey, _ := extKey2.Neuter()
+	for i, test := range tests {
+
+		for _, childNum := range test.path {
+			var err error
+			extKey, err = extKey.Derive(childNum)
+			require.NoError(t, err)
+
+			neuterKey, err = neuterKey.Derive(childNum)
+			require.NoError(t, err)
+		}
+
+		privStr, err := extKey.BLSPrivateKey()
+		require.NoError(t, err)
+		require.Equal(t, privStr.String(), test.wantPriv,
+			"mismatched serialized private key for test #%v", i+1)
+
+		pubStr, err := extKey.BLSPublicKey()
+		require.NoError(t, err)
+		require.Equal(t, pubStr.String(), test.wantPub,
+			"mismatched serialized public key for test #%v", i+1)
+
+		require.True(t, extKey.IsPrivate())
+		require.False(t, neuterKey.IsPrivate())
+		neuterPubStr, _ := extKey.BLSPublicKey()
+		require.Equal(t, neuterPubStr, pubStr)
+	}
+}
+
+// TestHardenedDerivation tests several vectors which derive private keys in
+// hardened mode
+func TestHardenedDerivation(t *testing.T) {
+	testSeed, _ := hex.DecodeString("c7195bf60f19a6ceee5bc4920ff6acf66d64126d743baa27bb9ead164c9b393dd3fc087ae9eafd83e32cb57f24dd653f5d4045dc5f45a4b461fb520cd8a10ca3")
+	hkStart := uint32(0x80000000)
+
+	tests := []struct {
+		name     string
+		path     []uint32
+		wantPriv string
+		wantPub  string
+	}{
+		// Test vector 1
+		{
+			name:     "test vector 1 chain m",
+			path:     []uint32{},
+			wantPriv: "SECRET1PPHL58434CM687WHZ0CDL2A36KDFRQ9GZS8SXJHX90E25W6DH6V3Q8SJRYL",
+			wantPub:  "public1pjhtucuyxzsmy526l5yjffj9ddt0lj05rhgk4rt7hyxrxhj27sdtc6qd8gxkseexlpfnjyd9jp2r7zqsy87mh72dtl9s2egmftkxgsc63rfj2yntedhz9xvx5smc8sqvhswzdv4a5zzmknzre4qz4cw2urupv25wf",
+		},
+		{
+			name:     "test vector 1 chain m/0H",
+			path:     []uint32{hkStart},
+			wantPriv: "SECRET1PGQ2F6J2DF25W2G5C7UKJKC9FKADFQT6R4DL7PJ4YLTFHVMEQFCCQCAMQVK",
+			wantPub:  "public1p3urj0ue8wty7yqsv6u8xl5yl2muznywdzxj8sfyga2e48r7hl6kvd28l35um8y23jn4vh6cl24mujz80clm39w57gu528lzk33wpdwpk4ftfgy5vyr5vwxxe506qepsdtv2refd253wwc80m5zrqjv0fyvrhvh7f",
+		},
+		{
+			name:     "test vector 1 chain m/0H/1",
+			path:     []uint32{hkStart, 1},
+			wantPriv: "SECRET1P9SLS00AZEVKQPYAEWJQG0X3KGA2WAEV83APCKUTMSDZFJ9MSZZUQ07VRGU",
+			wantPub:  "public1pjgdrhs9dtqsa8v0u8aqg7g3xmwnduxfe3cwfaqkmu7jygqeduuae2u2l9say5xszga2z3zvvuul0qxprwze5qlzf3ec5de5707e0sth94dn409t8y7rw66vjefv6aqnf5my5l0jjzzngqlw5yqc964j25y9w3fws",
+		},
+		{
+			name:     "test vector 1 chain m/0H/1/2H",
+			path:     []uint32{hkStart, 1, hkStart + 2},
+			wantPriv: "SECRET1PVS595W2ATQT8S3PTRNU5JSXWU0DKMYW8A9LHYK6CUDAUC7CPNJPSDYY75P",
+			wantPub:  "public1pspvarul3exq4v2pk54ryrx4pmqwg55avz85jhh63r0mnmtsmpk3r5a70jy0v99hth4j4ppjr9mg56rk4l9pnetpkyts7e6fv0vxmccpxrp9m3vjgwxhau7psrzrqv27klpgluqs9smztllhvmhykd39g7sus2yqy",
+		},
+		{
+			name:     "test vector 1 chain m/0H/1/2H/2",
+			path:     []uint32{hkStart, 1, hkStart + 2, 2},
+			wantPriv: "SECRET1PTGS4GXEDKNG82RQFTF5CTVWTRJH5RCNDV60JEWWW74EQR87QYNES2PRFNP",
+			wantPub:  "public1p3hn0zrackxprg00h9sgylw40j5a0qw45kv5z905rfn488f2wsq8r8yerargse3sy5uwkskz8t5wkvz23wxeescxny70nzp5g86ku9kn576gv8dvphz0gvm0puup9tzqce86tw740vchu6glxta46vdxz8ctz6k96",
+		},
+		{
+			name:     "test vector 1 chain m/0H/1/2H/2/1000000000",
+			path:     []uint32{hkStart, 1, hkStart + 2, 2, 1000000000},
+			wantPriv: "SECRET1PTTX080V6FMFWCECSCJNX366S8MG6USLH0UFCQVPUHSLW0WXLDJQQ9E2KYK",
+			wantPub:  "public1p3qsmjpzf3w3sfqs2wscxkqjzp7l3pnl7d7yhk032s52d039rw63tye55d5s3kchslefnh0jm09g2gq293ywnseqpdskzsqy8xslp9ulu9pqeh60h02594rl9g4uzl7qfjf4gdcmcl5r6nmvl2w5w5a6ur5x9ellk",
+		},
+	}
+
+	extKey, _ := NewMaster(testSeed)
+	for i, test := range tests {
+
+		for _, childNum := range test.path {
+			var err error
+			extKey, err = extKey.Derive(childNum)
+			require.NoError(t, err)
+		}
+
+		privStr, err := extKey.BLSPrivateKey()
+		require.NoError(t, err)
+		require.Equal(t, privStr.String(), test.wantPriv,
+			"mismatched serialized private key for test #%v", i+1)
+
+		pubStr, err := extKey.BLSPublicKey()
+		require.NoError(t, err)
+		require.Equal(t, pubStr.String(), test.wantPub,
+			"mismatched serialized public key for test #%v", i+1)
+
+		require.True(t, extKey.IsPrivate())
+	}
+}
+
+// TestGenerateSeed ensures the GenerateSeed function works as intended.
+func TestGenerateSeed(t *testing.T) {
+	tests := []struct {
+		name   string
+		length uint8
+		err    error
+	}{
+		// Test various valid lengths.
+		{name: "16 bytes", length: 16},
+		{name: "17 bytes", length: 17},
+		{name: "20 bytes", length: 20},
+		{name: "32 bytes", length: 32},
+		{name: "64 bytes", length: 64},
+
+		// Test invalid lengths.
+		{name: "15 bytes", length: 15, err: ErrInvalidSeedLen},
+		{name: "65 bytes", length: 65, err: ErrInvalidSeedLen},
+	}
+
+	for i, test := range tests {
+		seed, err := GenerateSeed(test.length)
+		assert.ErrorIs(t, err, test.err)
+
+		if test.err == nil && len(seed) != int(test.length) {
+			t.Errorf("GenerateSeed #%d (%s): length mismatch -- "+
+				"got %d, want %d", i, test.name, len(seed),
+				test.length)
+			continue
+		}
+	}
+}
