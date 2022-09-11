feeda87c9b98622cfcc273d6415361cb4b119c1e
ragnarok87 <38091008+nagarajmanjunath@users.noreply.github.com>
2022-09-11 19:33:50 +0800

    Added transaction event pubsub with test case (#385)

--- www/nanomsg/event/event.go
+++ www/nanomsg/event/event.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 
 	"github.com/pactus-project/pactus/crypto/hash"
+	"github.com/pactus-project/pactus/types/tx"
 	"github.com/pactus-project/pactus/util/encoding"
 	"github.com/pactus-project/pactus/util/logger"
 )
@@ -22,6 +23,19 @@ func CreateBlockEvent(blockHash hash.Hash, height uint32) Event {
 	err := encoding.WriteElements(w, TopicNewBlock, blockHash, height)
 	if err != nil {
 		logger.Error("error on encoding event", "err", err)
+	}
+	return w.Bytes()
+}
+
+// CreateBlockEvent creates an event when the new block is committed.
+// The block event structure is like :
+// <topic_id><tx_hash><height><sequence_number>
+func CreateNewTransactionEvent(txHash tx.ID, height uint32) Event {
+	buf := make([]byte, 0, 42)
+	w := bytes.NewBuffer(buf)
+	err := encoding.WriteElements(w, TopicNewTransaction, txHash, height)
+	if err != nil {
+		logger.Error("error on encoding event in transaction event", "err", err)
 	}
 	return w.Bytes()
 }
