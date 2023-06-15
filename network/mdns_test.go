d0474b1fe08b6bdbfe12fba0740e8f8a6db78b3b
b00f <mostafa.sedaghat@gmail.com>
2023-06-15 13:03:16 +0800

    fix: closing the mDNS connection upon stopping the network (#508)

--- network/mdns_test.go
+++ network/mdns_test.go
@@ -9,22 +9,24 @@ import (
 
 func TestMDNS(t *testing.T) {
 	conf1 := testConfig()
+	conf1.Listens = []string{"/ip4/127.0.0.1/tcp/0"}
 	conf1.EnableMdns = true
-	net1, err := NewNetwork(conf1)
-	assert.NoError(t, err)
+	net1, _ := newNetwork(conf1, nil)
 
 	conf2 := testConfig()
+	conf2.Listens = []string{"/ip4/127.0.0.1/tcp/0"}
 	conf2.EnableMdns = true
-	net2, err := NewNetwork(conf2)
-	assert.NoError(t, err)
+	net2, _ := newNetwork(conf2, nil)
 
 	assert.NoError(t, net1.Start())
+	time.Sleep(250 * time.Millisecond)
+
 	assert.NoError(t, net2.Start())
+	time.Sleep(250 * time.Millisecond)
 
-	assert.NoError(t, net1.JoinGeneralTopic())
-	assert.NoError(t, net2.JoinGeneralTopic())
-
-	time.Sleep(200 * time.Millisecond)
-
-	assert.NoError(t, net1.SendTo([]byte("test"), net2.SelfID()))
+	msg := []byte("test-mdns")
+	assert.NoError(t, net1.SendTo(msg, net2.SelfID()))
+	e := shouldReceiveEvent(t, net2).(*StreamMessage)
+	assert.Equal(t, e.Source, net1.SelfID())
+	assert.Equal(t, readData(t, e.Reader, len(msg)), msg)
 }
