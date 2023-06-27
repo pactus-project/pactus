1b4e5461f68516ccac60b7abac79e12764d1812f
Keyhan Alizadeh <kehiiiiya@gmail.com>
2023-06-27 07:04:58 -0500

    fix(HTTP): Missing handlers (#549)
    * feat: handlers and routes for getByNumber
    
    * test: test for getByNumberHandlers
    
    * fix: removed unecessary check and change tests
    
    * test: add missing test for non exist and move tests to new function

--- www/http/validator_test.go
+++ www/http/validator_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 
 	"github.com/gorilla/mux"
@@ -59,6 +60,65 @@ func TestValidator(t *testing.T) {
 		w := httptest.NewRecorder()
 		r := new(http.Request)
 		tHTTPServer.GetValidatorHandler(w, r)
+
+		assert.Equal(t, w.Code, 400)
+		fmt.Println(w.Body)
+	})
+}
+
+func TestValidatorByNumber(t *testing.T) {
+	setup(t)
+
+	val := tMockState.TestStore.AddTestValidator()
+
+	t.Run("Shall return a validator", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r := new(http.Request)
+		fmt.Println(val.Number())
+		fmt.Println(strconv.Itoa(int(val.Number())))
+		r = mux.SetURLVars(r, map[string]string{"number": strconv.Itoa(int(val.Number()))})
+		tHTTPServer.GetValidatorByNumberHandler(w, r)
+
+		assert.Equal(t, w.Code, 200)
+		fmt.Println(w.Body)
+	})
+
+	t.Run("Shall return a error, non exist", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r := new(http.Request)
+		fmt.Println(val.Number())
+		fmt.Println(strconv.Itoa(int(val.Number())))
+		r = mux.SetURLVars(r, map[string]string{"number": strconv.Itoa(int(val.Number() + 1))})
+		tHTTPServer.GetValidatorByNumberHandler(w, r)
+
+		assert.Equal(t, w.Code, 400)
+		fmt.Println(w.Body)
+	})
+
+	t.Run("Shall return an error, empty number", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r := new(http.Request)
+		r = mux.SetURLVars(r, map[string]string{"number": ""})
+		tHTTPServer.GetValidatorByNumberHandler(w, r)
+
+		assert.Equal(t, w.Code, 400)
+		fmt.Println(w.Body)
+	})
+
+	t.Run("Shall return an error, invalid number", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r := new(http.Request)
+		r = mux.SetURLVars(r, map[string]string{"number": "not-a-number"})
+		tHTTPServer.GetValidatorByNumberHandler(w, r)
+
+		assert.Equal(t, w.Code, 400)
+		fmt.Println(w.Body)
+	})
+
+	t.Run("Shall return an error, no number", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r := new(http.Request)
+		tHTTPServer.GetValidatorByNumberHandler(w, r)
 
 		assert.Equal(t, w.Code, 400)
 		fmt.Println(w.Body)
