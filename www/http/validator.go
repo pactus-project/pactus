3b89c19ee9c7fdc0717cd75e69006acd7a5d640d
kehiy <kehiiiiya@gmail.com>
2023-06-27 04:37:27 -0500

    feat: handlers and routes for getByNumber

--- www/http/validator.go
+++ www/http/validator.go
@@ -1,16 +1,58 @@
 package http
 
 import (
+	"fmt"
+	"math"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	pactus "github.com/pactus-project/pactus/www/grpc/gen/go"
 )
 
+// GetValidatorHandler returns a handler to get validator by address.
 func (s *Server) GetValidatorHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	res, err := s.blockchain.GetValidator(s.ctx,
 		&pactus.GetValidatorRequest{Address: vars["address"]})
+	if err != nil {
+		s.writeError(w, err)
+		return
+	}
+
+	val := res.Validator
+	tm := newTableMaker()
+	tm.addRowString("Public Key", val.PublicKey)
+	tm.addRowValAddress("Address", val.Address)
+	tm.addRowInt("Number", int(val.Number))
+	tm.addRowInt("Sequence", int(val.Sequence))
+	tm.addRowAmount("Stake", val.Stake)
+	tm.addRowInt("LastBondingHeight", int(val.LastBondingHeight))
+	tm.addRowInt("LastJoinedHeight", int(val.LastJoinedHeight))
+	tm.addRowInt("UnbondingHeight", int(val.UnbondingHeight))
+	tm.addRowBytes("Hash", val.Hash)
+
+	s.writeHTML(w, tm.html())
+}
+
+// GetValidatorByNumberHandler returns a handler to get validator by number.
+func (s *Server) GetValidatorByNumberHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	num, err := strconv.ParseInt(vars["number"], 10, 32)
+	if err != nil {
+		s.writeError(w, err)
+		return
+	}
+
+	if num > math.MaxInt32 || num < math.MinInt32 {
+		s.writeError(w, fmt.Errorf("integer overflow detected"))
+		return
+	}
+
+	res, err := s.blockchain.GetValidatorByNumber(s.ctx, &pactus.GetValidatorByNumberRequest{
+		Number: int32(num),
+	})
 	if err != nil {
 		s.writeError(w, err)
 		return
