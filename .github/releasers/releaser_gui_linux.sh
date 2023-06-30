0c94ae142da2db6e254c469e98e309a87214c88b
mantre <mantre@protonmail.com>
2023-06-30 23:14:32 +0800

    chore: statically link release binaries

--- .github/releasers/releaser_gui_linux.sh
+++ .github/releasers/releaser_gui_linux.sh
@@ -15,8 +15,8 @@ mkdir ${PACKAGE_DIR}
 echo "Building the binaries"
 
 cd ${ROOT_DIR}
-go build -ldflags "-s -w" -trimpath -o ${BUILD_DIR}/pactus-daemon ./cmd/daemon
-go build -ldflags "-s -w" -trimpath -o ${BUILD_DIR}/pactus-wallet ./cmd/wallet
+CGO_ENABLED=0 go build -ldflags "-s -w" -trimpath -o ${BUILD_DIR}/pactus-daemon ./cmd/daemon
+CGO_ENABLED=0 go build -ldflags "-s -w" -trimpath -o ${BUILD_DIR}/pactus-wallet ./cmd/wallet
 go build -ldflags "-s -w" -trimpath -tags gtk -o ${BUILD_DIR}/pactus-gui ./cmd/gtk
 
 # Moving binaries to package directory
