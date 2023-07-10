8e564d9899a0941be162c1443ba7a269ad982971
Mostafa <mostafa.sedaghat@gmail.com>
2023-07-10 22:35:07 +0800

    fix: set block hash on hello message

--- sync/handler_hello.go
+++ sync/handler_hello.go
@@ -2,7 +2,6 @@ package sync
 
 import (
 	"github.com/libp2p/go-libp2p/core/peer"
-	"github.com/pactus-project/pactus/crypto/hash"
 	"github.com/pactus-project/pactus/sync/bundle"
 	"github.com/pactus-project/pactus/sync/bundle/message"
 	"github.com/pactus-project/pactus/sync/peerset"
@@ -49,7 +48,7 @@ func (handler *helloHandler) ParseMessage(m message.Message, initiator peer.ID) 
 		msg.Agent,
 		msg.PublicKey,
 		util.IsFlagSet(msg.Flags, message.FlagNodeNetwork))
-	handler.peerSet.UpdateHeight(initiator, msg.Height, hash.UndefHash)
+	handler.peerSet.UpdateHeight(initiator, msg.Height, msg.BlockHash)
 
 	if !util.IsFlagSet(msg.Flags, message.FlagHelloAck) {
 		// TODO: Sends response only if there is a direct connection between two peers.
