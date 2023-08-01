55ea7a52b5e396f52a7773c13b0a0ff4db43b741
kehiy <kehiiiiya@gmail.com>
2023-08-01 17:31:27 +0800

    feat: added trusted peers to firewall config

--- sync/firewall/config.go
+++ sync/firewall/config.go
@@ -1,12 +1,16 @@
 package firewall
 
+import "github.com/libp2p/go-libp2p/core/peer"
+
 type Config struct {
-	Enabled bool `toml:"enable"`
+	Enabled      bool      `toml:"enable"`
+	TrustedPeers []peer.ID `toml:"trusted_peers"`
 }
 
 func DefaultConfig() *Config {
 	return &Config{
 		Enabled: false,
+		TrustedPeers: []peer.ID{},
 	}
 }
 
