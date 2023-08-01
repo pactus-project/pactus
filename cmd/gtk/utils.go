de74d077bea0523ca4ffdc588253ccf5875665de
zigBalthazar <zigbalthazar@gmail.com>
2023-08-01 13:29:33 +0330

    fix:remove unused import

--- cmd/gtk/utils.go
+++ cmd/gtk/utils.go
@@ -16,7 +16,6 @@ import (
 	"github.com/pactus-project/pactus/types/tx"
 	"github.com/pactus-project/pactus/types/tx/payload"
 	"github.com/pactus-project/pactus/util"
-	"github.com/pactus-project/pactus/util/logger"
 	"github.com/pactus-project/pactus/wallet"
 )
 
