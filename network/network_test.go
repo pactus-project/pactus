23a627b68de1e8e1bbf05f7aec71f5bd91399931
Mostafa <mostafa.sedaghat@gmail.com>
2023-09-02 02:55:41 +0800

    test: using 0.0.0.0 address

--- network/network_test.go
+++ network/network_test.go
@@ -338,12 +338,12 @@ func TestConnections(t *testing.T) {
 		bootstrapAddr string
 		peerAddr      string
 	}{
-		{"/ip4/127.0.0.1/tcp/%d", "/ip4/127.0.0.1/tcp/0"},
+		{"/ip4/0.0.0.0/tcp/%d", "/ip4/0.0.0.0/tcp/0"},
 		{"/ip6/::/tcp/%d", "/ip6/::/tcp/0"},
-		{"/ip4/127.0.0.1/tcp/%d", "/ip6/::/tcp/0"},
-		{"/ip4/127.0.0.1/udp/%d/quic", "/ip6/::/udp/0/quic"},
+		{"/ip4/0.0.0.0/tcp/%d", "/ip6/::/tcp/0"},
+		{"/ip4/0.0.0.0/udp/%d/quic", "/ip6/::/udp/0/quic"},
 		{"/ip6/::/udp/%d/quic", "/ip6/::/tcp/0"},
-		{"/ip4/127.0.0.1/tcp/%d", "/ip4/127.0.0.1/udp/0/quic"},
+		{"/ip4/0.0.0.0/tcp/%d", "/ip4/0.0.0.0/udp/0/quic"},
 	}
 
 	for i, test := range tests {
