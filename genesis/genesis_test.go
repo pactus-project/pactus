858f6a5d325a0df2fb00cb4944fb7130d4f53ba9
Mantre <mantre@protonmail.com>
2023-09-30 14:10:19 +0800

    chore: updating genesis for Pre-Testnet-2 (#717)

--- genesis/genesis_test.go
+++ genesis/genesis_test.go
@@ -54,8 +54,8 @@ func TestGenesisTestNet(t *testing.T) {
 
 	assert.Equal(t, g.Accounts()[crypto.TreasuryAddress].Balance(), int64(21e15))
 
-	genTime, _ := time.Parse("2006-01-02", "2023-09-07")
-	expected, _ := hash.FromString("b7285501eee807780a8ab62e60b8248597b155260c118b5a76b586b00e9cfaa9")
+	genTime, _ := time.Parse("2006-01-02", "2023-09-28")
+	expected, _ := hash.FromString("7d360d11517748531bd3ce22937615d6629c3972d56d21e848549b3e762e342c")
 	assert.Equal(t, g.Hash(), expected)
 	assert.Equal(t, g.GenesisTime(), genTime)
 	assert.Equal(t, g.Params().BondInterval, uint32(120))
