e4fbcd6aa54ed77f23a58a3a80b4184cfe823cf8
Mostafa <mostafa.sedaghat@gmail.com>
2023-10-09 22:52:37 +0800

    fix: removing the support of UDP

--- tests/main_test.go
+++ tests/main_test.go
@@ -83,7 +83,7 @@ func TestMain(m *testing.M) {
 		tConfigs[i].Sync.Firewall.Enabled = false
 		tConfigs[i].Network.EnableMdns = true
 		tConfigs[i].Network.NetworkKey = util.TempFilePath()
-		tConfigs[i].Network.Listens = []string{"/ip4/127.0.0.1/tcp/0", "/ip4/127.0.0.1/udp/0/quic"}
+		tConfigs[i].Network.Listens = []string{"/ip4/127.0.0.1/tcp/0"}
 		tConfigs[i].Network.Bootstrap.Addresses = []string{}
 		tConfigs[i].Network.Bootstrap.Period = 10 * time.Second
 		tConfigs[i].Network.Bootstrap.MinThreshold = 3
