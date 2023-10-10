8c40984e03e749a9f85e3eb33f55f73c39ecee59
Mostafa <mostafa.sedaghat@gmail.com>
2023-10-10 11:48:33 +0800

    test: fixing firewall tests

--- sync/sync.go
+++ sync/sync.go
@@ -296,9 +296,6 @@ func (sync *synchronizer) sendTo(msg message.Message, to peer.ID) error {
 
 		err := sync.network.SendTo(data, to)
 		if err != nil {
-			sync.logger.Warn("error on sending bundle",
-				"bundle", bdl, "to", to, "error", err)
-
 			return err
 		}
 		sync.logger.Info("sending bundle to a peer",
