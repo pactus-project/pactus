56d61aee7f80285d39441890450a9b2f75517c3d
IZIK <84680298+izikdepth@users.noreply.github.com>
2024-01-08 18:58:52 +0100

    feat(http): adding AvailabilityScore to http module (#917)

--- www/http/blockchain_test.go
+++ www/http/blockchain_test.go
@@ -265,3 +265,22 @@ func TestValidatorByNumber(t *testing.T) {
 		fmt.Println(w.Body)
 	})
 }
+
+func TestConsensusInfo(t *testing.T) {
+	td := setup(t)
+
+	h, _ := td.mockConsMgr.HeightRound()
+	v1, _ := td.GenerateTestPrepareVote(h, 1)
+	v2, _ := td.GenerateTestPrecommitVote(h, 2)
+	td.mockConsMgr.AddVote(v1)
+	td.mockConsMgr.AddVote(v2)
+
+	w := httptest.NewRecorder()
+	r := new(http.Request)
+
+	td.httpServer.ConsensusHandler(w, r)
+
+	assert.Equal(t, w.Code, 200)
+	assert.Contains(t, w.Body.String(), "<td>2</td>")
+	assert.Contains(t, w.Body.String(), v2.Signer().String())
+}
