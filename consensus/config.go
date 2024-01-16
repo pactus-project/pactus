0ed070f776e018630d63d9c8c389c51abc9dd9f9
Kay <kehiiiiya@gmail.com>
2024-01-16 17:39:21 +0000

    chore: adding pip19 score to consensus config, turning consensus to a zero-config module

--- consensus/config.go
+++ consensus/config.go
@@ -7,14 +7,16 @@ import (
 )
 
 type Config struct {
-	ChangeProposerTimeout time.Duration `toml:"change_proposer_timeout"`
-	ChangeProposerDelta   time.Duration `toml:"change_proposer_delta"`
+	ChangeProposerTimeout    time.Duration `toml:"-"`
+	ChangeProposerDelta      time.Duration `toml:"-"`
+	MinimumAvailabilityScore float64       `toml:"-"`
 }
 
 func DefaultConfig() *Config {
 	return &Config{
-		ChangeProposerTimeout: 6 * time.Second,
-		ChangeProposerDelta:   2 * time.Second,
+		ChangeProposerTimeout:    8 * time.Second,
+		ChangeProposerDelta:      4 * time.Second,
+		MinimumAvailabilityScore: 0.8,
 	}
 }
 
@@ -25,6 +27,9 @@ func (conf *Config) BasicCheck() error {
 	}
 	if conf.ChangeProposerDelta <= 0 {
 		return errors.Errorf(errors.ErrInvalidConfig, "change proposer delta can't be negative")
+	}
+	if conf.MinimumAvailabilityScore < 0 || conf.MinimumAvailabilityScore > 1 {
+		return errors.Errorf(errors.ErrInvalidConfig, "minimum availability score can't be negative or more than 1")
 	}
 
 	return nil
