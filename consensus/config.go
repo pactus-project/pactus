1aa034c9dbd824809d4dd9f565cf1d2ca1c0d177
Kay <kehiiiiya@gmail.com>
2024-01-17 08:53:03 +0000

    feat(consensus): turning consensus to a zero-config module (#942)

--- consensus/config.go
+++ consensus/config.go
@@ -1,30 +1,37 @@
 package consensus
 
-import (
-	"time"
-
-	"github.com/pactus-project/pactus/util/errors"
-)
+import "time"
 
 type Config struct {
-	ChangeProposerTimeout time.Duration `toml:"change_proposer_timeout"`
-	ChangeProposerDelta   time.Duration `toml:"change_proposer_delta"`
+	ChangeProposerTimeout    time.Duration `toml:"-"`
+	ChangeProposerDelta      time.Duration `toml:"-"`
+	MinimumAvailabilityScore float64       `toml:"-"`
 }
 
 func DefaultConfig() *Config {
 	return &Config{
-		ChangeProposerTimeout: 6 * time.Second,
-		ChangeProposerDelta:   2 * time.Second,
+		ChangeProposerTimeout:    8 * time.Second,
+		ChangeProposerDelta:      4 * time.Second,
+		MinimumAvailabilityScore: 0.9,
 	}
 }
 
 // BasicCheck performs basic checks on the configuration.
 func (conf *Config) BasicCheck() error {
 	if conf.ChangeProposerTimeout <= 0 {
-		return errors.Errorf(errors.ErrInvalidConfig, "timeout for change proposer can't be negative")
+		return ConfigError{
+			Reason: "timeout for change proposer can't be negative",
+		}
 	}
 	if conf.ChangeProposerDelta <= 0 {
-		return errors.Errorf(errors.ErrInvalidConfig, "change proposer delta can't be negative")
+		return ConfigError{
+			Reason: "change proposer delta can't be negative",
+		}
+	}
+	if conf.MinimumAvailabilityScore < 0 || conf.MinimumAvailabilityScore > 1 {
+		return ConfigError{
+			Reason: "minimum availability score can't be negative or more than 1",
+		}
 	}
 
 	return nil
