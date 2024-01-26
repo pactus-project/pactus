c2bd8c3a7b0cdab6ce2d95d70979845d8a816037
amirvalhalla <amirvalhalla@proton.me>
2024-01-26 20:37:38 +0330

    feat: change default parameters to custom parametrs in cobra

--- cmd/ctl/cmd/root.go
+++ cmd/ctl/cmd/root.go
@@ -9,16 +9,8 @@ import (
 // rootCmd represents the base command when called without any subcommands.
 var rootCmd = &cobra.Command{
 	Use:   "ctl",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Pactus control cli",
+	Long:  `Managing the Pactus node using CLI.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -31,11 +23,5 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
