d83c23ef098d062d86212a3c459d9179dd8fcf4e
amirvalhalla <amirvalhalla@proton.me>
2024-02-13 17:23:52 +0330

    feat: implement insert into transaction table

--- wallet2/db.go
+++ wallet2/db.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"errors"
 	"strings"
+	"time"
 
 	//nolint:all
 	_ "github.com/glebarez/go-sqlite"
@@ -17,10 +18,33 @@ var (
 
 type DB interface {
 	CreateTables() error
+	InsertIntoAddress(addr, pubKey, label, path string) error
+	InsertIntoTransaction(t *Transaction) error
 }
 
 type db struct {
 	*sql.DB
+}
+
+type Address struct {
+	ID        string `json:"id"`         // id of wallet
+	Address   string `json:"address"`    // Address in the wallet
+	PublicKey string `json:"public_key"` // Public key associated with the address
+	Label     string `json:"label"`      // Label for the address
+	Path      string `json:"path"`       // Path for the address
+}
+
+type Transaction struct {
+	ID          string    `json:"id"`
+	TxID        string    `json:"tx_id"`
+	BlockHeight uint32    `json:"block_height"`
+	BlockTime   uint32    `json:"block_time"`
+	PayloadType string    `json:"payload_type"`
+	Data        string    `json:"data"`
+	Description string    `json:"description"`
+	Amount      int64     `json:"amount"`
+	Status      int       `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
 }
 
 func newDB(path string) (DB, error) {
@@ -60,7 +84,7 @@ func (d *db) createAddressTable() error {
 func (d *db) createTransactionTable() error {
 	transactionQuery := "CREATE TABLE transactions (id INTEGER PRIMARY KEY AUTOINCREMENT," +
 		" tx_id VARCHAR, block_height INTEGER, block_time INTEGER, payload_type VARCHAR," +
-		" data VARCHAR, description VARCHAR, amount BIGINT,created_at TIMESTAMP)"
+		" data VARCHAR, description VARCHAR, amount BIGINT,status INTEGER, created_at TIMESTAMP)"
 	_, err := d.ExecContext(context.Background(), transactionQuery)
 	if err != nil && !strings.Contains(err.Error(), "already exists") {
 		return ErrCouldNotCreateTable
@@ -77,4 +101,23 @@ func (d *db) createPairTable() error {
 	}
 
 	return nil
+}
+
+func (d *db) InsertIntoAddress(addr, pubKey, label, path string) error {
+	insertQuery := "INSERT INTO addresses (address, public_key, label, path) VALUES (?,?,?,?)"
+	_, err := d.ExecContext(context.Background(), insertQuery, addr, pubKey, label, path)
+
+	return err
+}
+
+func (d *db) InsertIntoTransaction(t *Transaction) error {
+	insertQuery := "INSERT INTO transactions (tx_id, block_height, block_time," +
+		" payload_type, data, description, amount, status, created_at) VALUES" +
+		" (?,?,?,?,?,?,?,?,?)"
+
+	t.CreatedAt = time.Now().UTC()
+	_, err := d.ExecContext(context.Background(), insertQuery, t.TxID, t.BlockHeight, t.BlockTime,
+		t.PayloadType, t.Data, t.Description, t.Amount, t.Status, t.CreatedAt)
+
+	return err
 }
--- wallet2/db_test.go
+++ wallet2/db_test.go
@@ -1,14 +1,32 @@
 package wallet2
 
 import (
+	"log"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNewDB(t *testing.T) {
-	someDB, err := newDB(":memory:")
+	someDB, err := newDB("wallet.db")
+	// someDB, err := newDB(":memory:")
 
 	assert.Nil(t, err)
 	assert.NotNil(t, someDB)
+
+	someDB.CreateTables()
+	err = someDB.InsertIntoAddress("test", "test", "test", "test")
+	log.Println(err)
+	tran := &Transaction{
+		TxID:        "ssf",
+		BlockHeight: 2,
+		BlockTime:   3,
+		PayloadType: "sf",
+		Data:        "sf",
+		Description: "sfd",
+		Amount:      3,
+		Status:      1,
+	}
+	err = someDB.InsertIntoTransaction(tran)
+	log.Println(err)
 }
