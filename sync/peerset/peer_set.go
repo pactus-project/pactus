29f3eacb79c3be0ddc3209391a4de5081e096d9c
mantre <mantre@protonmail.com>
2024-02-14 21:31:17 +0800

    fix(sync): fix concurrent map read-write crash

--- sync/peerset/peer_set.go
+++ sync/peerset/peer_set.go
@@ -404,6 +404,9 @@ func (ps *PeerSet) StartedAt() time.Time {
 }
 
 func (ps *PeerSet) IteratePeers(consumer func(peer *Peer) (stop bool)) {
+	ps.lk.RLock()
+	defer ps.lk.RUnlock()
+
 	for _, p := range ps.peers {
 		stopped := consumer(p)
 		if stopped {
@@ -412,13 +415,17 @@ func (ps *PeerSet) IteratePeers(consumer func(peer *Peer) (stop bool)) {
 	}
 }
 
-func (ps *PeerSet) IterateSessions(consumer func(s *session.Session) (stop bool)) {
+func (ps *PeerSet) Sessions() []*session.Session {
+	ps.lk.RLock()
+	defer ps.lk.RUnlock()
+
+	sessions := make([]*session.Session, 0, len(ps.sessions))
+
 	for _, ssn := range ps.sessions {
-		stopped := consumer(ssn)
-		if stopped {
-			return
-		}
+		sessions = append(sessions, ssn)
 	}
+
+	return sessions
 }
 
 // GetRandomPeer selects a random peer from the peer set based on their weights.
