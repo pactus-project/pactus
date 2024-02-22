0201133479c4069ae762404e1aafee7eb092ee01
laosiji-io <laosiji-io@outlook.com>
2024-02-23 00:33:49 +0800

    build: craete AppImage for GUI in linux

--- .github/releasers/releaser_gui_linux.sh
+++ .github/releasers/releaser_gui_linux.sh
@@ -27,3 +27,21 @@ mv ${BUILD_DIR}/pactus-daemon  ${PACKAGE_DIR}/pactus-daemon
 
 echo "Creating archive"
 tar -czvf ${ROOT_DIR}/${PACKAGE_NAME}_linux_amd64.tar.gz -p ${PACKAGE_NAME}
+
+# building AppImage
+# https://github.com/linuxdeploy/linuxdeploy-plugin-gtk
+
+cp ${ROOT_DIR}/.github/releasers/linux/*    ${PACKAGE_DIR}
+cp ${ROOT_DIR}/.github/releasers/pactus.png ${PACKAGE_DIR}
+
+cd ${PACKAGE_DIR}
+
+wget -c "https://raw.githubusercontent.com/linuxdeploy/linuxdeploy-plugin-gtk/master/linuxdeploy-plugin-gtk.sh"
+wget -c "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage"
+
+chmod +x linuxdeploy-x86_64.AppImage linuxdeploy-plugin-gtk.sh
+
+DEPLOY_GTK_VERSION=3 ./linuxdeploy-x86_64.AppImage -e ./pactus-gui --appdir AppDir --plugin gtk --output appimage --icon-file pactus.png --desktop-file ./pactus.desktop
+
+cp ./pactus-gui-x86_64.AppImage ${ROOT_DIR}/${PACKAGE_NAME}_linux_amd64.AppImage
+
