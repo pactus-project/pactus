f2d6338cc04d9ef8942dbdc8430775fde79e1219
Kay <kehiiiiya@gmail.com>
2024-03-26 10:32:12 +0000

    Update dialog_transaction_transfer.go

--- cmd/gtk/dialog_transaction_transfer.go
+++ cmd/gtk/dialog_transaction_transfer.go
@@ -31,7 +31,7 @@ func broadcastTransactionTransfer(wlt *wallet.Wallet) {
 
 	for _, i := range wlt.AllAccountAddresses() {
 		senderEntry.Append(i.Address, i.Address)
-	}	
+	}
 	senderEntry.SetActive(0)
 
 	onSenderChanged := func() {
