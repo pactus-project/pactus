a514b47735f1b28f0314253c6d0c8248163c840c
b00f <mostafa.sedaghat@gmail.com>
2024-06-10 13:04:09 +0800

    fix(consensus): implement PIP-26 (#1331)

--- www/grpc/gateway.go
+++ www/grpc/gateway.go
@@ -123,7 +123,7 @@ func allowCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if origin := r.Header.Get("Origin"); origin != "" {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
-			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
+			if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
 				preflightHandler(w)
 
 				return
