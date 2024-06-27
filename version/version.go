8a2056c5b5eade086deb36524129f95ad02cc511
Mantre <mantre@protonmail.com>
2024-06-27 18:00:02 +0800

    chore: releasing version 1.3.0 (#1377)

--- version/version.go
+++ version/version.go
@@ -16,8 +16,8 @@ var NodeVersion = Version{
 	Major: 1,
 	Minor: 3,
 	Patch: 0,
-	Meta:  "beta",
-	Alias: "",
+	Meta:  "",
+	Alias: "Warsaw",
 }
 
 // Version defines the version of Pactus software.
