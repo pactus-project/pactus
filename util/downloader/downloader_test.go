d4eac28012fbef3cde07b0fcff84d5bdc56ecccc
Javad <ja7ad@live.com>
2024-07-17 11:03:30 +0330

    fix: used http test server for download test

--- util/downloader/downloader_test.go
+++ util/downloader/downloader_test.go
@@ -2,25 +2,18 @@ package downloader
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
 	"log"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"path/filepath"
-	"sync"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 )
-
-func setup() *Downloader {
-	fileURL := "https://github.com/pactus-project/Whitepaper/releases/latest/download/pactus_whitepaper.pdf"
-	filePath := "./testdata/example.pdf"
-	expectedSHA256 := "ea956128717b49669f29eeed116bc11b9bbdcd50f1df130e124ffd36afe71652"
-
-	dl := New(fileURL, filePath, expectedSHA256)
-
-	return dl
-}
 
 func cleanup(path string) error {
 	err := os.Remove(path)
@@ -37,23 +30,35 @@ func cleanup(path string) error {
 }
 
 func TestDownloader(t *testing.T) {
-	dl := setup()
+	fileContent := []byte("This is a test file content")
+	fileURL := "/testfile"
+	expectedSHA256 := sha256.Sum256(fileContent)
+	expectedSHA256Hex := hex.EncodeToString(expectedSHA256[:])
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == fileURL {
+			_, err := w.Write(fileContent)
+			assert.NoError(t, err)
+		} else {
+			http.NotFound(w, r)
+		}
+	}))
+	defer server.Close()
+
+	filePath := "./testdata/example_testfile.txt"
+
+	defer func() {
+		assert.NoError(t, os.RemoveAll("./testdata"))
+	}()
+
+	dl := New(server.URL+fileURL, filePath, expectedSHA256Hex, WithCustomClient(server.Client()))
 
 	assrt := assert.New(t)
-	var wg sync.WaitGroup
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
 	defer cancel()
 
-	wg.Add(1)
 	go func() {
-		defer wg.Done()
-		dl.Start()
-	}()
-
-	go func() {
-		<-ctx.Done()
-		dl.Stop()
-		assrt.Fail("Download test timed out")
+		dl.Start(ctx)
 	}()
 
 	done := make(chan bool)
@@ -86,48 +91,16 @@ func TestDownloader(t *testing.T) {
 	select {
 	case <-done:
 	case <-time.After(2 * time.Minute):
-		dl.Stop()
+		cancel()
 		assrt.Fail("Download test timed out")
 	}
 
 	t.Log(dl.FileName())
 	t.Log(dl.FileType())
 
+	downloadedContent, err := os.ReadFile(filePath)
+	assrt.NoError(err, "Failed to read the downloaded file")
+	assrt.Equal(fileContent, downloadedContent, "Downloaded file content does not match expected content")
+
 	assert.NoError(t, cleanup(dl.filePath))
-
-	wg.Wait()
-}
-
-func TestDownloaderOperations(t *testing.T) {
-	dl := setup()
-	dl.Start()
-
-	assrt := assert.New(t)
-
-	go func() {
-		for stat := range dl.Stats() {
-			log.Printf("Downloaded: %d / %d (%.2f%%)\n", stat.Downloaded, stat.TotalSize, stat.Percent)
-			assrt.True(stat.Downloaded <= stat.TotalSize, "Downloaded size should not exceed total size")
-			assrt.True(stat.Percent <= 100, "Download percentage should not exceed 100")
-
-			if stat.Completed {
-				log.Println("Download completed successfully")
-				assrt.Equal(float64(100), stat.Percent, "Download should be 100% complete")
-
-				return
-			}
-		}
-	}()
-
-	time.Sleep(1 * time.Second)
-	dl.Pause()
-	t.Log("Paused")
-	time.Sleep(3 * time.Second)
-	dl.Resume()
-	t.Log("Resumed")
-
-	time.Sleep(1 * time.Second)
-	dl.Stop()
-
-	assrt.NoError(cleanup(dl.filePath))
 }
