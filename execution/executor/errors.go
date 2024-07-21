7d93f479ad34cc50f7496c374441a03b3c6efa53
Mostafa <mostafa.sedaghat@gmail.com>
2024-07-21 22:53:57 +0800

    refactor(execution): change error messages

--- execution/executor/errors.go
+++ execution/executor/errors.go
@@ -9,14 +9,14 @@ import (
 	"github.com/pactus-project/pactus/types/tx/payload"
 )
 
-// ErrInsufficientFunds indicates the balance is low for the transaction.
+// ErrInsufficientFunds indicates that the balance is insufficient for the transaction.
 var ErrInsufficientFunds = errors.New("insufficient funds")
 
 // ErrPublicKeyNotSet indicates that the public key is not set for the initial Bond transaction.
 var ErrPublicKeyNotSet = errors.New("public key is not set")
 
 // ErrPublicKeyAlreadySet indicates that the public key has already been set for the given validator.
-var ErrPublicKeyAlreadySet = errors.New("public key is set before")
+var ErrPublicKeyAlreadySet = errors.New("public key is already set")
 
 // ErrValidatorBonded indicates that the validator is bonded.
 var ErrValidatorBonded = errors.New("validator is bonded")
@@ -24,29 +24,33 @@ var ErrValidatorBonded = errors.New("validator is bonded")
 // ErrValidatorUnbonded indicates that the validator has unbonded.
 var ErrValidatorUnbonded = errors.New("validator has unbonded")
 
-// ErrBondingPeriod is returned when a validator is in bonding period.
+// ErrBondingPeriod is returned when a validator is in the bonding period.
 var ErrBondingPeriod = errors.New("validator in bonding period")
 
-// ErrUnbondingPeriod is returned when a validator is in unbonding period.
+// ErrUnbondingPeriod is returned when a validator is in the unbonding period.
 var ErrUnbondingPeriod = errors.New("validator in unbonding period")
 
-// ErrInvalidSortitionProof indicates the sortition proof is invalid.
+// ErrInvalidSortitionProof indicates that the sortition proof is invalid.
 var ErrInvalidSortitionProof = errors.New("invalid sortition proof")
 
-// ErrExpiredSortition indicates the sortition transaction is duplicated or expired.
+// ErrExpiredSortition indicates that the sortition transaction is duplicated or expired.
 var ErrExpiredSortition = errors.New("expired sortition")
 
 // ErrValidatorInCommittee indicates that the validator is in the committee.
 var ErrValidatorInCommittee = errors.New("validator is in the committee")
 
-// ErrCommitteeJoinLimitExceeded indicates that more than 1/3 of the committee stake's can join the committee.
-var ErrCommitteeJoinLimitExceeded = errors.New("in each height only 1/3 of stake can join the committee")
+// ErrCommitteeJoinLimitExceeded indicates that at each height,
+// the maximum stake joining the committee can't be more than 1/3 of the committee's total stake.
+var ErrCommitteeJoinLimitExceeded = errors.New(
+	"the maximum stake joining the committee can't be more than 1/3 of the committee's total stake")
 
-// ErrCommitteeLeaveLimitExceeded indicates that more than 1/3 of the committee stake's can leave the committee.
-var ErrCommitteeLeaveLimitExceeded = errors.New("in each height only 1/3 of stake can leave the committee")
+// ErrCommitteeLeaveLimitExceeded indicates that at each height,
+// the maximum stake leaving the committee can't be more than 1/3 of the committee's total stake.
+var ErrCommitteeLeaveLimitExceeded = errors.New(
+	"the maximum stake leaving the committee can't be more than 1/3 of the committee's total stake")
 
-// ErrOldestValidatorNotProposed indicates that the oldest validator still didn't propose any block.
-var ErrOldestValidatorNotProposed = errors.New("oldest validator still didn't propose any block")
+// ErrOldestValidatorNotProposed indicates that the oldest validator has not proposed any block yet.
+var ErrOldestValidatorNotProposed = errors.New("oldest validator has not proposed any block yet")
 
 // SmallStakeError is returned when the stake amount is less than the minimum stake.
 type SmallStakeError struct {
@@ -66,15 +70,13 @@ func (e MaximumStakeError) Error() string {
 	return fmt.Sprintf("validator's stake amount can't be more than %v", e.Maximum.String())
 }
 
-// InvalidPayloadTypeError is returned when transaction payload type
-// is not valid.
+// InvalidPayloadTypeError is returned when the transaction payload type is not valid.
 type InvalidPayloadTypeError struct {
 	PayloadType payload.Type
 }
 
 func (e InvalidPayloadTypeError) Error() string {
-	return fmt.Sprintf("unknown payload type: %s",
-		e.PayloadType.String())
+	return fmt.Sprintf("unknown payload type: %s", e.PayloadType.String())
 }
 
 // AccountNotFoundError is raised when the given address has no associated account.
