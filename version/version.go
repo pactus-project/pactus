75ef2d10e3277b3c27c451d8ea25474a9b952b4e
mantre <mantre@protonmail.com>
2024-08-01 23:58:41 +0800

    chore(version): bumping version to 1.5.0

--- version/version.go
+++ version/version.go
@@ -14,10 +14,10 @@ import (
 // During development, set the Meta field to "beta".
 var NodeVersion = Version{
 	Major: 1,
-	Minor: 4,
+	Minor: 5,
 	Patch: 0,
-	Meta:  "",
-	Alias: "Amsterdam",
+	Meta:  "beta",
+	Alias: "",
 }
 
 // Version defines the version of Pactus software.
