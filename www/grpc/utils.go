902426438b13d0313b21b8d590fa006a3d209d03
alidevjimmy <ali.hamrani2001@gmail.com>
2024-11-06 10:53:10 +0330

    add aggregate methods

--- www/grpc/utils.go
+++ www/grpc/utils.go
@@ -2,6 +2,7 @@ package grpc
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/pactus-project/pactus/crypto/bls"
 	pactus "github.com/pactus-project/pactus/www/grpc/gen/go"
@@ -55,5 +56,43 @@ func (*utilServer) VerifyMessage(_ context.Context,
 
 	return &pactus.VerifyMessageResponse{
 		IsValid: false,
+	}, nil
+}
+
+func (*utilServer) BLSPublicKeyAggregate(_ context.Context,
+	req *pactus.BLSPublicKeyAggregateRequest,
+) (*pactus.BLSPublicKeyAggregateResponse, error) {
+	if len(req.PublicKeys) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "no public keys provided")
+	}
+	pubs := make([]*bls.PublicKey, len(req.PublicKeys))
+	for i, pubKey := range req.PublicKeys {
+		p, err := bls.PublicKeyFromString(pubKey)
+		if err != nil {
+			return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("invalid public key %s", pubKey))
+		}
+		pubs[i] = p
+	}
+	return &pactus.BLSPublicKeyAggregateResponse{
+		PublicKey: bls.PublicKeyAggregate(pubs...).String(),
+	}, nil
+}
+
+func (*utilServer) BLSSignatureAggregate(_ context.Context,
+	req *pactus.BLSSignatureAggregateRequest,
+) (*pactus.BLSSignatureAggregateResponse, error) {
+	if len(req.Signatures) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "no signatures provided")
+	}
+	sigs := make([]*bls.Signature, len(req.Signatures))
+	for i, sig := range req.Signatures {
+		s, err := bls.SignatureFromString(sig)
+		if err != nil {
+			return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("invalid signature %s", sig))
+		}
+		sigs[i] = s
+	}
+	return &pactus.BLSSignatureAggregateResponse{
+		Signature: bls.SignatureAggregate(sigs...).String(),
 	}, nil
 }
