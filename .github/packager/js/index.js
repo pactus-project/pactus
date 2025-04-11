668512b71d984a7f8196a57d10974186e2c87235
Danh Nguyen <nguyendanhtravel@gmail.com>
2025-04-11 18:44:28 +0700

    chore: rm filter sub-folder since it's redundant

--- .github/packager/js/index.js
+++ .github/packager/js/index.js
@@ -10,21 +10,6 @@ try {
       const moduleName = path.basename(file, '.js');
       modules[moduleName] = require(`./${file}`);
     });
-  
-  fs.readdirSync(__dirname)
-    .filter(dir => {
-      const dirPath = path.join(__dirname, dir);
-      return fs.existsSync(dirPath) && fs.statSync(dirPath).isDirectory();
-    })
-    .forEach(dir => {
-      modules[dir] = {};
-      fs.readdirSync(path.join(__dirname, dir))
-        .filter(file => file.endsWith('.js'))
-        .forEach(file => {
-          const moduleName = path.basename(file, '.js');
-          modules[dir][moduleName] = require(`./${dir}/${file}`);
-        });
-    });
 } catch (err) {
   console.error('Error loading gRPC modules:', err);
 }
