bfd32feb72bd22ad3dea488681e163b7df439d5d
Danh Nguyen <nguyendanhtravel@gmail.com>
2025-04-11 18:35:39 +0700

    fix: cmd gen proto on different os (Makefile, auto-gen)

--- .github/packager/packager.sh
+++ .github/packager/packager.sh
@@ -4,6 +4,14 @@
 # if any command returns a non-zero exit status (i.e., an error).
 set -e
 
+replace_in_place() {
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' "$1" "$2"
+  else
+    sed -i "$1" "$2"
+  fi
+}
+
 ROOT_DIR="$(pwd)"
 PACKAGE_DIR="${ROOT_DIR}/packages"
 GEN_DIR="${ROOT_DIR}/www/grpc/gen"
@@ -15,8 +23,7 @@ mkdir -p ${PACKAGE_DIR}
 
 echo "== Building pactus-grpc package for JavaScript"
 cp -R ${ROOT_DIR}/.github/packager/js ${PACKAGE_DIR}/js
-mkdir -p ${PACKAGE_DIR}/js/pactus_grpc
-cp -R ${GEN_DIR}/js/* ${PACKAGE_DIR}/js/pactus_grpc/
+cp -R ${GEN_DIR}/js/* ${PACKAGE_DIR}/js/
 cp ${ROOT_DIR}/LICENSE ${PACKAGE_DIR}/js/
 cp ${ROOT_DIR}/README.md ${PACKAGE_DIR}/js/
-sed -i '' "s/{{ VERSION }}/$VERSION/g" ${PACKAGE_DIR}/js/package.json
+replace_in_place "s/{{ VERSION }}/$VERSION/g" "${PACKAGE_DIR}/js/package.json"
