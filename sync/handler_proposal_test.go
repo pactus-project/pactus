323bf379787d546e86a0a344e949c81ee39ab8fe
b00f <mostafa.sedaghat@gmail.com>
2025-09-06 15:03:53 +0800

    fix(state): update protocol version for own validators (#1871)

--- sync/handler_proposal_test.go
+++ sync/handler_proposal_test.go
@@ -4,13 +4,15 @@ import (
 	"testing"
 
 	"github.com/pactus-project/pactus/sync/bundle/message"
+	"github.com/pactus-project/pactus/types/protocol"
+	"github.com/pactus-project/pactus/util/testsuite"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestParsingProposalMessages(t *testing.T) {
 	td := setup(t, nil)
 
-	t.Run("Parsing proposal message", func(t *testing.T) {
+	t.Run("Set proposal for consensus", func(t *testing.T) {
 		consensusHeight := td.state.LastBlockHeight() + 1
 		prop := td.GenerateTestProposal(consensusHeight, 0)
 		msg := message.NewProposalMessage(prop)
@@ -18,5 +20,20 @@ func TestParsingProposalMessages(t *testing.T) {
 
 		td.receivingNewMessage(td.sync, msg, pid)
 		assert.Equal(t, prop, td.consMgr.Proposal())
+	})
+
+	t.Run("Update protocol version of the proposer", func(t *testing.T) {
+		consensusHeight := td.state.LastBlockHeight() + 1
+		valKey := td.RandValKey()
+		val := td.GenerateTestValidator(testsuite.ValidatorWithPublicKey(valKey.PublicKey()))
+		td.state.TestStore.UpdateValidator(val)
+		prop := td.GenerateTestProposal(consensusHeight, 0, testsuite.ProposalWithKey(valKey))
+		msg := message.NewProposalMessage(prop)
+		pid := td.RandPeerID()
+
+		td.receivingNewMessage(td.sync, msg, pid)
+
+		assert.Equal(t, protocol.ProtocolVersionLatest,
+			td.state.ValidatorByAddress(valKey.Address()).ProtocolVersion())
 	})
 }
