ef86e8dcff1d7728db8116b81e287b03ceab986e
mantre <mantre@protonmail.com>
2026-01-15 01:54:22 +0800

    chore(util): use ezeX testsuite library

--- util/testsuite/testsuite.go
+++ util/testsuite/testsuite.go
@@ -4,11 +4,11 @@ import (
 	"context"
 	"encoding/hex"
 	"fmt"
-	"math/rand"
 	"net"
 	"testing"
 	"time"
 
+	"github.com/ezex-io/gopkg/testsuite"
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/pactus-project/pactus/committee"
 	"github.com/pactus-project/pactus/crypto"
@@ -37,8 +37,7 @@ import (
 // by re-evaluating all the random values. This helps in identifying and debugging
 // failures in testing conditions.
 type TestSuite struct {
-	Seed int64
-	Rand *rand.Rand
+	testsuite.TestSuite
 	Ctrl *gomock.Controller
 }
 
@@ -54,10 +53,8 @@ func NewTestSuiteFromSeed(t *testing.T, seed int64) *TestSuite {
 	t.Cleanup(ctrl.Finish)
 
 	return &TestSuite{
-		Seed: seed,
-		//nolint:gosec // to reproduce the failed tests
-		Rand: rand.New(rand.NewSource(seed)),
-		Ctrl: ctrl,
+		TestSuite: *testsuite.NewTestSuiteFromSeed(t, seed),
+		Ctrl:      ctrl,
 	}
 }
 
@@ -73,91 +70,6 @@ func NewTestSuite(t *testing.T) *TestSuite {
 
 func (ts *TestSuite) MockingController() *gomock.Controller {
 	return ts.Ctrl
-}
-
-// RandBool returns a random boolean value.
-func (ts *TestSuite) RandBool() bool {
-	return ts.RandInt64(2) == 0
-}
-
-// RandInt8 returns a random int8 between 0 and max: [0, max).
-func (ts *TestSuite) RandInt8(max int8) int8 {
-	return int8(ts.RandUint64(uint64(max)))
-}
-
-// RandUint8 returns a random uint8 between 0 and max: [0, max).
-func (ts *TestSuite) RandUint8(max uint8) uint8 {
-	return uint8(ts.RandUint64(uint64(max)))
-}
-
-// RandInt16 returns a random int16 between 0 and max: [0, max).
-func (ts *TestSuite) RandInt16(max int16) int16 {
-	return int16(ts.RandUint64(uint64(max)))
-}
-
-// RandUint16 returns a random uint16 between 0 and max: [0, max).
-func (ts *TestSuite) RandUint16(max uint16) uint16 {
-	return uint16(ts.RandUint64(uint64(max)))
-}
-
-// RandInt32 returns a random int32 between 0 and max: [0, max).
-func (ts *TestSuite) RandInt32(max int32) int32 {
-	return int32(ts.RandUint64(uint64(max)))
-}
-
-// RandUint32 returns a random uint32 between 0 and max: [0, max).
-func (ts *TestSuite) RandUint32(max uint32) uint32 {
-	return uint32(ts.RandUint64(uint64(max)))
-}
-
-// RandInt64 returns a random int64 between 0 and max: [0, max).
-func (ts *TestSuite) RandInt64(max int64) int64 {
-	return ts.Rand.Int63n(max)
-}
-
-// RandUint64 returns a random uint64 between 0 and max: [0, max).
-func (ts *TestSuite) RandUint64(max uint64) uint64 {
-	return uint64(ts.RandInt64(int64(max)))
-}
-
-// RandInt returns a random int between 0 and max: [0, max).
-func (ts *TestSuite) RandInt(max int) int {
-	return int(ts.RandInt64(int64(max)))
-}
-
-// RandInt16NonZero returns a random int16 between 1 and max+1: [1, max+1).
-func (ts *TestSuite) RandInt16NonZero(max int16) int16 {
-	return ts.RandInt16(max) + 1
-}
-
-// RandUint16NonZero returns a random uint16 between 1 and max+1: [1, max+1).
-func (ts *TestSuite) RandUint16NonZero(max uint16) uint16 {
-	return ts.RandUint16(max) + 1
-}
-
-// RandInt32NonZero returns a random int32 between 1 and max+1: [1, max+1).
-func (ts *TestSuite) RandInt32NonZero(max int32) int32 {
-	return ts.RandInt32(max) + 1
-}
-
-// RandUint32NonZero returns a random uint32 between 1 and max+1: [1, max+1).
-func (ts *TestSuite) RandUint32NonZero(max uint32) uint32 {
-	return ts.RandUint32(max) + 1
-}
-
-// RandInt64NonZero returns a random int64 between 1 and max+1: [1, max+1).
-func (ts *TestSuite) RandInt64NonZero(max int64) int64 {
-	return ts.RandInt64(max) + 1
-}
-
-// RandUint64NonZero returns a random uint64 between 1 and max+1: [1, max+1).
-func (ts *TestSuite) RandUint64NonZero(max uint64) uint64 {
-	return ts.RandUint64(max) + 1
-}
-
-// RandIntNonZero returns a random int between 1 and max+1: [1, max+1).
-func (ts *TestSuite) RandIntNonZero(max int) int {
-	return ts.RandInt(max) + 1
 }
 
 // RandHeight returns a random number between [1000, 1000000] for block height.
@@ -183,9 +95,7 @@ func (ts *TestSuite) RandAmount(max ...amount.Amount) amount.Amount {
 
 // RandAmountRange returns a random amount between [min, max).
 func (ts *TestSuite) RandAmountRange(min, max amount.Amount) amount.Amount {
-	amt := amount.Amount(ts.RandInt64NonZero(int64(max - min)))
-
-	return amt + min
+	return amount.Amount(ts.RandInt64Range(min.ToNanoPAC(), max.ToNanoPAC()))
 }
 
 // RandFee returns a random fee between [1e7, max).
