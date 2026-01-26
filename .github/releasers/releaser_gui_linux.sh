c9f526aa27b0a39464ee5115d95925f437bd2d23
Mostafa <mostafa.sedaghat@gmail.com>
2026-01-26 17:35:06 +0800

    ci(releaser): set CGO_ENABLED for building Pactus-GUI

--- .github/releasers/releaser_gui_linux.sh
+++ .github/releasers/releaser_gui_linux.sh
@@ -28,7 +28,7 @@ cd ${ROOT_DIR}
 CGO_ENABLED=0 go build -ldflags "-s -w" -trimpath -o ${BUILD_DIR}/pactus-daemon ./cmd/daemon
 CGO_ENABLED=0 go build -ldflags "-s -w" -trimpath -o ${BUILD_DIR}/pactus-wallet ./cmd/wallet
 CGO_ENABLED=0 go build -ldflags "-s -w" -trimpath -o ${BUILD_DIR}/pactus-shell ./cmd/shell
-go build -ldflags "-s -w" -trimpath -tags gtk -o ${BUILD_DIR}/pactus-gui ./cmd/gtk
+CGO_ENABLED=1 go build -ldflags "-s -w" -trimpath -tags gtk -o ${BUILD_DIR}/pactus-gui ./cmd/gtk
 
 # Moving binaries to package directory
 echo "Moving binaries"
