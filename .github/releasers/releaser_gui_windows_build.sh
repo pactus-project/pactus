5edb37ddc75dec1d934bcb7b6660bca21daa7d64
b00f <mostafa.sedaghat@gmail.com>
2026-01-27 13:31:23 +0800

    ci(releaser): set CGO_ENABLED for building Pactus-GUI (#2058)

--- .github/releasers/releaser_gui_windows_build.sh
+++ .github/releasers/releaser_gui_windows_build.sh
@@ -14,4 +14,4 @@ sed -i -e 's/-Wl,-luuid/-luuid/g' /mingw64/lib/pkgconfig/gdk-3.0.pc
 CGO_ENABLED=0 go build -ldflags "-s -w" -trimpath -o ${BUILD_DIR}/unsigned/pactus-daemon.exe        ./cmd/daemon
 CGO_ENABLED=0 go build -ldflags "-s -w" -trimpath -o ${BUILD_DIR}/unsigned/pactus-wallet.exe        ./cmd/wallet
 CGO_ENABLED=0 go build -ldflags "-s -w" -trimpath -o ${BUILD_DIR}/unsigned/pactus-shell.exe         ./cmd/shell
-go build -ldflags "-s -w -H windowsgui" -trimpath -tags gtk -o ${BUILD_DIR}/unsigned/pactus-gui.exe ./cmd/gtk
+CGO_ENABLED=1 go build -ldflags "-s -w -H windowsgui" -trimpath -tags gtk -o ${BUILD_DIR}/unsigned/pactus-gui.exe ./cmd/gtk
