7b55c37bd68021eff00c4b0594d4ee0b6923acf1
Mostafa <mostafa.sedaghat@gmail.com>
2026-04-15 16:24:11 +0800

    chore(wallet): remove todo comment

--- wallet/manager/errors.go
+++ wallet/manager/errors.go
@@ -8,7 +8,6 @@ var (
 	// ErrWalletAlreadyExists indicates a wallet already exists on disk.
 	ErrWalletAlreadyExists = errors.New("wallet already exists")
 
-	// TODO: rename me
 	// ErrWalletNotLoaded indicates a wallet is not loaded in memory.
 	ErrWalletNotLoaded = errors.New("wallet is not loaded")
 )
