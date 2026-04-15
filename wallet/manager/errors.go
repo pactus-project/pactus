d4d0243ddb2d75d8ca34651b4e02c11dda0eb33b
Mostafa <mostafa.sedaghat@gmail.com>
2026-04-15 12:33:43 +0800

    feat(wallet): define wallet config for locking mode

--- wallet/manager/errors.go
+++ wallet/manager/errors.go
@@ -12,12 +12,3 @@ var (
 	// ErrWalletNotLoaded indicates a wallet is not loaded in memory.
 	ErrWalletNotLoaded = errors.New("wallet is not loaded")
 )
-
-// ConfigError is returned when the config is not valid with a descriptive Reason message.
-type ConfigError struct {
-	Reason string
-}
-
-func (e ConfigError) Error() string {
-	return e.Reason
-}
