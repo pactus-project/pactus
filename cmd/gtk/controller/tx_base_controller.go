2290dbbcc71d8a88b83a89d9c30f139a95683fb9
Mantre <mantre@protonmail.com>
2026-05-26 18:25:02 +0800

    refactor(gtk): use confirmAndSend method for transaction dilaogs (#2233)

---
+++ cmd/gtk/controller/tx_base_controller.go
@@ -0,0 +1,42 @@
+//go:build gtk
+
+package controller
+
+import (
+	"fmt"
+
+	"github.com/gotk3/gotk3/gtk"
+	"github.com/pactus-project/pactus/cmd/gtk/gtkutil"
+	"github.com/pactus-project/pactus/cmd/gtk/model"
+	"github.com/pactus-project/pactus/types/tx"
+)
+
+func confirmAndSend(parent gtk.IWindow, model *model.WalletModel, msg string, trx *tx.Tx) bool {
+	if !gtkutil.ShowQuestionDialog(parent, msg) {
+		return false
+	}
+
+	password, ok := PasswordProvider(model)
+	if !ok {
+		return false
+	}
+
+	if err := model.SignTransaction(password, trx); err != nil {
+		gtkutil.ShowError(err)
+
+		return false
+	}
+
+	txID, err := model.BroadcastTransaction(trx)
+	if err != nil {
+		gtkutil.ShowError(err)
+
+		return false
+	}
+
+	gtkutil.ShowInfoDialog(parent,
+		fmt.Sprintf("✅ Transaction sent successfully!\n\n"+
+			"Transaction ID: <a href=\"https://pactusscan.com/transaction/%s\">%s</a>", txID, txID))
+
+	return true
+}
